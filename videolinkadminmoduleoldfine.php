5dc3b2e28cccb948d979ea615d10c855f2c009af
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-08 16:56:40 -0500

    2

--- videolinkadminmoduleoldfine.php
+++ videolinkadminmoduleoldfine.php
@@ -51,7 +51,7 @@ include "sessions.php"
 				<form action="savelinks.php" method="POST"> 
 				
 					<div class="form-group">
-					<font color=lightblue id="form_title"><strong>Video URL full link</strong></font></center><br>	
+						<font color=black id="form_title"><strong>Video URL full link</strong></font><br>	
 						<textarea name="videolink" rows="5" class="form-control" placeholder="Video URL full link"></textarea> <br>
 					</div>
 					<div class="form-group">
