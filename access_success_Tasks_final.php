39b77584b85880e6b85ac1ab6d25dccfd60d1e82
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-11 18:20:42 -0500

    localusername

--- access_success_Tasks_final.php
+++ access_success_Tasks_final.php
@@ -2,17 +2,18 @@
 
 <?php 
 
-GLOBAL $usernamer;/*10072024*/
+GLOBAL $global_username;/*10072024*/
 /*if (session_status() === PHP_SESSION_NONE) {*/
     session_start();
 /*}*/	
-$usernamer=$_POST['username'];
+$global_username=$_POST['username'];
+$_SESSION['username']=$global_username;/*POST veriable assinged to global session usernamer global10112024*/
+$local_username=$_SESSION['username'];/*10082024*/
 $password=$_POST['password'];
-/*$_SESSION['usernamer']=$username;10112024*/
 $count=0;
 $_SESSION['counter']=$count;
 $counter=$_SESSION['counter'];
-/*$usernamer=$_SESSION['username'];/*10082024*/
+
 
 	
 	if($_POST)
@@ -34,7 +35,7 @@ $counter=$_SESSION['counter'];
 		{
 
 		}
-		$query1="select * from videotips_app_access_list where username='$usernamer' and active='1' and password='$password'"; /*if username is user role 10082024*/
+		$query1="select * from videotips_app_access_list where username='$local_username' and active='1' and password='$password'"; /*if username is user role 10082024*/
 		$result1=mysqli_query($conn, $query1);
 	
 		if(mysqli_num_rows($result1)==true)
