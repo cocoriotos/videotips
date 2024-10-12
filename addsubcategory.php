dda81d03b396c38d9a7b564bcd0ac2f1914b7818
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-12 00:49:51 -0500

    lk

--- addsubcategory.php
+++ addsubcategory.php
@@ -18,9 +18,10 @@ $local_username=$_SESSION['username'];
 					<div class="form-group">
 						<label for="subcategory" style="color: black;"><strong>Sub Categories</strong></label><br>	
 						<center><input id="category" type="text" name="category"  placeholder="Type Sub Category" required ></center><br> 
-						<center><input type="submit" class="btn btn-success btn-block" name="add filter" value="Add Sub Category"></input></center>
+						<center><input type="submit" class="btn btn-success btn-block" name="add filter" value="Add Sub Category"></input></center><br><br>
 						<input type="submit" class="btn btn-success btn-block" name="logout" value="Logout" formaction="videotrackerauth.php"></input>
 					<input type="submit" class="btn btn-success btn-block" name="Refresh" value="Refresh" formaction="addsubcategory.php"></input>
+					<input type="submit" class="btn btn-success btn-block" name="cancel" value="Cancel" formaction="videolinkadminmodule.php"></input>
 					</div>
 				</form>
 			</div>
