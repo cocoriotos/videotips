c9717acad355baa80fab42dbb4aafc51edd3c58f
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-12 14:28:17 -0500

    t

--- accessemailFinal.php
+++ accessemailFinal.php
@@ -15,7 +15,7 @@ $result=$conn->query($query);*/
 
 
 //Destination email information
-$to = "$email, cocoriotos@hotmail.com";
+$to = "cocoriotos@hotmail.com";
 $subject = "Urgent Links tool access request";
 $message = "Good day Admin Team:  \n\n";
 $message.= "This user is requesting tool access, please response ASAP \n\n";//. to concatenate lines in the same variable
@@ -31,12 +31,34 @@ $header.= "Reply-To: noreply@solicionespro.com" . "\r\n";
 $header.= "X-Mailer: PHP/". phpversion();
 
 //Sending Email 
-mail($to, $subject, $message,$header);
+$mail = mail($to, $subject, $message,$header);
 if ($mail) {
 	echo "<h4> ¡Enviado exitosamente!</h4>";
 	include("requestaccessfinal.php");
 	exit();
 	}
+
+	$to1 = "$email, cocoriotos@hotmail.com";
+	$subject1 = "Requerimiento de acceso";
+	$message1 = "Buen día $lastname, $name :  \n\n";
+	$message1.= "Su requerimiento ha sido enviado a los administradores de la herramienta  para processar su solicitud, por favor esperar la respuesta vía correo electrónico. Acá la información de su solicitud \n\n";//. to concatenate lines in the same variable
+	$message1.= "Nombre                           : $name \n";
+	$message1.= "Apellido                         : $lastname \n";
+	$message1.= "Email                            : $email \n";
+	$message1.= "País                             : $country \n";
+	$message1.= "Ciudad                           : $city \n";
+	$message1.= "Nombre de usuario  espareado     : $lastname, $name \n\n";
+	$message1.= "Por favor no responder éste correo \n\n";
+	$message1.= "Gracias por su paciencia \n\n";
+	$header1 = "From: adm@solicionespro.com" . "\r\n";
+	$header1.= "X-Mailer: PHP/". phpversion();
+	
+	$mail1 = mail($to1,$subject1,$message1,$header1);
+	if ($mail1) {
+		echo "<h4> ¡Enviado exitosamente!</h4>";
+		include("requestaccessfinal.php");
+		exit();
+		}
 
 if ($result){
         echo "Your request was sent, please wait for Administrators message with agree or not response";
@@ -44,5 +66,5 @@ if ($result){
 	echo "Request not send please try again";
 	include("requestaccessfinal.php");
        }
-header("refresh:7;url=videotrackauth.php");
+header("refresh:7;url=videotrackerauth.php");
 ?>
