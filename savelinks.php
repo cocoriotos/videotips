e9a4f4e872fba4ee80cf60cebec834f36586e09b
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-11 21:38:27 -0500

    duplicidad

--- savelinks.php
+++ savelinks.php
@@ -13,19 +13,17 @@ $description=$_POST["description"];
 $query="INSERT INTO videotips_videotips (videolink,maincategory,category,description,active,username) values ('$videolink','$maincategory','$category','$description','Yes','$local_username')";
 $resultado= $conn ->query($query);
 
-if ($conn->errno == 1062){
-  echo "Error: La dirección que está tratando de ingresar ya existe";
-  header("refresh:3; url=videolinkadminmodule.php");
-}
+
 
 
 if ($resultado){
+  if ($conn->errno == 1062){
+    echo "Error: La dirección que está tratando de ingresar ya existe";
+    header("refresh:3; url=videolinkadminmodule.php");
+  } else{
   $_SESSION['message']='Link Saved Successfully';
   $_SESSION['message_type']='Success';
   header("refresh:3; url=videolinkadminmodule.php");
-    }
-  else{
-      echo 	"Link Not Saved";
-      header("refresh:3; url=videolinkadminmodule.php");
-      }
+  }
+  }
 ?>
