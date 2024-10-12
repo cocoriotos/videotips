392b0fd835dbbd26ca54f8a7e3131d6d9fe4abc7
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-11 19:04:53 -0500

    updates

--- updatelinks.php
+++ updatelinks.php
@@ -10,11 +10,10 @@ $maincategory=$_POST["maincategory"];
 $category=$_POST["category"];
 $description=$_POST["description"];
 $active=$_POST["active"];
-$usernamer=$_SESSION['usernamer'];
 $_SESSION['counter']=$_SESSION['counter']+1;
 
 
-$query="UPDATE videotips_videotips SET id = '$id', videolink = '$videolink', maincategory = '$maincategory', category = '$category', description = '$description', active = '$active', username = '$local_username' where id = '$id'";
+$query="UPDATE videotips_videotips SET id = '$id', videolink = '$videolink', maincategory = '$maincategory', category = '$category', description = '$description', active = '$active' where id = '$id'";
 $resultado=$conn ->query($query);
 
 if ($resultado){
