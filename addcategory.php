9d564ab9d14afdf1a7af9d5633694e1a4c045311
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-12 08:33:53 -0500

    a

--- addcategory.php
+++ addcategory.php
@@ -52,7 +52,7 @@ $local_username=$_SESSION['username'];
 					while($categories = mysqli_fetch_array($result_categories)) { ?>
 					  <tr>
 					     <td align="center" onclick="Display"><?php echo"<a href='categiries.php?id={$categories['maincategory']}'>{$categories['maincategory']}"?></td>
-						 <td align="center" onclick="Display"><?php echo"<a href='categiries.php?id={$categories['category']}'>{$categories['category']}"?></td>
+						 <td align="center" onclick="Display"><?php echo $categories['category']?></td>
 					 	 </td>
 					  </tr>
 					<?php }?>
