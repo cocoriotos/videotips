3982b55da4b882f0316d7b6ccf4a53e1f220e57f
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-13 07:39:15 -0500

    validation

--- savecategory.php
+++ savecategory.php
@@ -10,22 +10,19 @@ $category=$_POST["category"];
 $query="SELECT categorycounter from  videotips_app_access_list where username = '$local_username'";
 $categorycounter= $conn ->query($query);
  
-if ($categorycounter === false) {
-  // Si la consulta falla, puedes mostrar el error
-  echo "Error en la consulta: " . $conn->error;
-} else {
-  echo "Consulta ejecutada correctamente.";
+{
+  // Acción a realizar si es numérico y mayor a 5
 }
-?>
 
-/*if ($categorycounter > 5){
+
+if(is_numeric($categorycounter) && $categorycounter > 5) {
   echo ("You reach the 5 free subcategories registration, to continue adding more, please see our plans to");
   $_SESSION['message']='Subcategories not saved Successfully';
   $_SESSION['message_type']='No Success';
   header("refresh:7; url=addcategory.php");
 }
 
-if ($categorycounter < 6){
+if(is_numeric($categorycounter) && $categorycounter < 6) {
   $query1="INSERT INTO videotips_viodetipscategory (maincategory, category, username) values ('$maincategory','$category','$local_username')";
   $resultado1= $conn ->query($query1);
   $query2="INSERT INTO videotips_maincategory (maincategory, username) values ('$maincategory', '$local_username')";
@@ -39,4 +36,4 @@ if ($categorycounter < 6){
       header("refresh:7; url=addcategory.php");
       } 
     }
-?>*/
+?>
