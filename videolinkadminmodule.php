76ee9d2dbf10db044eae48c8e62bc6f37fb1b10c
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-13 10:14:25 -0500

    account

--- videolinkadminmodule.php
+++ videolinkadminmodule.php
@@ -8,6 +8,48 @@ include "db_connection1.php";
 $local_username=$_SESSION['username'];
 ?>
 
+
+<div class="container p-4">
+	<div class="row">
+		<div class="col-md-4"> 
+			<div class="card card-body">
+				<form class="" action="savelinks.php" method="POST"> 
+				
+					<div class="form-group">
+						<label for="videolink" style="color: black;"><strong>Video URL full link</strong></label><br>	
+						<textarea name="videolink" rows="5" class="form-control" placeholder="Video URL full link"></textarea> <br>
+					</div>
+					<div class="form-group">
+						<label for="maincategory" style="color: black;"><strong>Category</strong></label><br>	
+						<select name= "maincategory" required> <?php $SQLSELECT = "SELECT * FROM videotips_maincategory order by maincategory asc"; $result_set = mysqli_query($conn, $SQLSELECT); while ($rows = $result_set ->fetch_assoc()) { $maincategory = $rows['maincategory']; echo "<option value='$maincategory'>$maincategory</option>";} ?></select> <br><br>
+					</div>
+					<div class="form-group">
+						<label for="category" style="color: black;"><strong>Subcategory</strong></label><br>	
+						<select name= "category" required> <?php $SQLSELECT = "SELECT * FROM videotips_viodetipscategory order by category asc"; $result_set =  mysqli_query($conn, $SQLSELECT); while ($rows = $result_set ->fetch_assoc()) { $category = $rows['category']; echo "<option value='$category'>$category</option>";} ?></select> <br><br>
+					</div>
+	                 <div class="form-group">
+					 	<label for="description" style="color: black;"><strong>Description</strong></label><br>	
+						<textarea name="description" rows="5" class="form-control" placeholder="Video tip Description"></textarea> <br>
+					</div>
+					<div class="form-group">
+					 	<label for="useful" style="color: black;"><strong>Useful</strong></label><br>	
+						 <select name= "active" required> <?php $SQLSELECT = "SELECT * FROM videotips_active order by active desc"; $result_set =  mysqli_query($conn, $SQLSELECT); while ($rows = $result_set ->fetch_assoc()) { $active = $rows['active']; echo "<option value='$active'>$active</option>";} ?></select> <br><br>
+					</div>  	
+					<br>
+					<input type="submit" class="btn btn-success btn-block" name="save_link" value="Save Link"></input></center>
+					<input type="submit" class="btn btn-success btn-block" name="logout" value="Logout" formaction="videotrackerauth.php"></input>
+					<input type="submit" class="btn btn-success btn-block" name="logout" value="Refresh" formaction="videolinkadminmodule.php"></input>
+					<input type="submit" class="btn btn-success btn-block" name="logout" value="Categories" formaction="addcategory.php"></input></center>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
 <div class="container p-4">
 	<div class="row">
 		<div class="col-md-4"> 
