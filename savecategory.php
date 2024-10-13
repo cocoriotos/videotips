c35dfb2b85ee26523f62bfe0fd685e446c3fc07c
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-13 11:59:12 -0500

    f

--- savecategory.php
+++ savecategory.php
@@ -46,7 +46,7 @@ if (is_numeric($categorycounter) && $categorycounter > 5 && $extendcounterfeatur
     exit(); // Importante para detener la ejecución después de redirigir
 }
 
-if ((is_numeric($categorycounter) && $categorycounter <= 5) || (is_numeric($categorycounter) && $categorycounter > 5 && $extendcounterfeature === 1) || ((is_string($role) === 'Admin')))  {
+if ((is_numeric($categorycounter) && $categorycounter <= 5) || (is_numeric($categorycounter) && $categorycounter > 5 && $extendcounterfeature === 1) || ((is_string($role) && $role === 'Admin')))  {
     // Inserta la nueva categoría y maincategory
     $query1 = "INSERT INTO videotips_viodetipscategory (maincategory, category, username) VALUES (?, ?, ?)";
     $stmt1 = $conn->prepare($query1);
