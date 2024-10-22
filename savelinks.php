201722e9d52db8615099e09ae52d04d55d487bf8
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-22 17:52:02 -0500

    c

--- savelinks.php
+++ savelinks.php
@@ -13,7 +13,7 @@ $description=$_POST["description"];
 $query="SELECT suscriptionactive from  videotips_app_access_list where username = '$local_username'";
 $activesuscription= $conn ->query($query);
 
-$query1="SELECT videolink from  videotips_videotips where videolink = '$videolink' and username = '$local_username'";
+$query1="SELECT * from  videotips_videotips where videolink = '$videolink' and username = '$local_username'";
 $urlduplicated= $conn ->query($query1);
 
 
@@ -27,27 +27,6 @@ if($activesuscription == 1){
 $query="INSERT INTO videotips_videotips (videolink,maincategory,category,description,active,username) values ('$videolink','$maincategory','$category','$description','Yes','$local_username')";
 $resultado= $conn ->query($query);
 }
-/*Avoid duplicated URLs*/
-
-/*try {
-  if ($urlduplicated === TRUE) {
-      echo "Registro insertado exitosamente";
-  } else {
-      throw new Exception($conn->error, $conn->errno);
-  }
-} catch (Exception $e) {
-  // Verificar si el código de error es 1062 (duplicidad)
-  if ($e->getCode() == 1062) {
-      echo "Error: Ya existe una página igual registrada. Por favor, usa otra.";
-      exit;
-  } else {
-      // Para cualquier otro error
-      echo "Error: Hay un error. Por favor, intentar nuevamente.";
-      exit;
-  }
-}*/
-
-
 
 if ($resultado){
     $_SESSION['message']='Link Saved Successfully';
