bab23ae6289cb721a44030545432c18e2b2f5a4b
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-26 12:49:47 -0500

    d

--- savelinks.php
+++ savelinks.php
@@ -23,19 +23,16 @@ if($urlduplicated->num_rows > 0){
   header("refresh:3; url=videolinkadminmodule.php");
   exit();
 } else {
-if($activesuscription ->num_rows > 0){
-
+if($activesuscription == 1){
 $query3="INSERT INTO videotips_videotips (videolink,maincategory,category,description,active,username) values ('$videolink','$maincategory','$category','$description','Yes','$local_username')";
 $resultado= $conn ->query($query3);
-
 $query4="INSERT INTO videotips_app_access_list (categorycounter) values ('categorycounter++')";
 $resultado1= $conn ->query($query4);
-
-
 }
 
 if ($resultado){
-    $_SESSION['message']='Link Saved Successfully';
+  echo ("Link Saved Successfully");
+  $_SESSION['message']='Link Saved Successfully';
   $_SESSION['message_type']='Success';
   
   header("refresh:3; url=videolinkadminmodule.php");
