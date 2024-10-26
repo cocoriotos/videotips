9a0053e25deae16527e306772f13599bfa9bcaac
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-26 13:26:01 -0500

    center

--- editcategory.php
+++ editcategory.php
@@ -33,8 +33,8 @@ $local_username=$_SESSION['email'];
 						<label for="videolink" style="color: black;"><strong>Sub Category</strong></label><br>	
 						<input type="text" name="category" class="form-control" placeholder="link" autofocus value ="<?php echo $link['category'];?>"></input><br>
 					</div>
-					<input type="submit" class="btn btn-success btn-block" name="update_category" value="Update"></input>
-					<input type="submit" class="btn btn-success btn-block" name="logout" value="Delete" formaction="deletecategory.php"></input>
+					<input type="submit" class="btn btn-success btn-block" name="update_category" value="Update"></input><br>
+					<input type="submit" class="btn btn-success btn-block" name="logout" value="Delete" formaction="deletecategory.php"></input><br>
 					<input type="submit" class="btn btn-success btn-block" name="logout" value="Cancel" formaction="addcategory.php"></input>
 				</form>
 			</div>
