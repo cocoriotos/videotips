3ddc440d8a05f9b9c6412fac2263f479371881c6
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-26 14:02:56 -0500

    i

--- editcategory.php
+++ editcategory.php
@@ -1,4 +1,4 @@
-<form?php include "header.php";
+<?php include "header.php";
 include "db_connection1.php";
 session_start();
 $id = $_GET['id'];
@@ -33,10 +33,10 @@ $local_username=$_SESSION['email'];
 						<label for="videolink" style="color: black;"><strong>Sub Category</strong></label><br>	
 						<input type="text" name="category" class="form-control" placeholder="link" autofocus value ="<?php echo $link['category'];?>"></input><br>
 					</div>
-					<input type="submit" class="btn btn-success btn-block" name="update_category" value="Update"></input><br><br><br>
-					<input type="submit" class="btn btn-success btn-block" name="logout" value="Delete" formaction="deletecategory.php"></input><br><br><br>
-					<input type="submit" class="btn btn-success btn-block" name="logout" value="Cancel" formaction="addcategory.php"></input><br><br>
-				</form>
+					<center><input type="submit" class="btn btn-success btn-block" name="update_category" value="Update"></input></center><br><br><br>
+					<center></center><input type="submit" class="btn btn-success btn-block" name="logout" value="Delete" formaction="deletecategory.php"></input></center><br><br><br>
+					<center><input type="submit" class="btn btn-success btn-block" name="logout" value="Cancel" formaction="addcategory.php"></input></center><br><br>
+				</c>
 			</div>
 		</div>
 		
