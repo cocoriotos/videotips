3e3d8e3aa822fe6dbd46cd7eac9485db14ea2b02
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-27 10:53:33 -0500

    e

--- savecategory.php
+++ savecategory.php
@@ -35,9 +35,9 @@ $stmt->bind_result($categoryresult);
 $stmt->fetch();
 $stmt->close();
 
-print_r("$categorycounter");
-print_r("$extendcounterfeature");
-print_r("$categoryresult");
+echo("$categorycounter");
+echo("$extendcounterfeature");
+echo("$categoryresult");
 
 /*
 if($categoryresult){ 
