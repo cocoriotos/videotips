b9d96b555332553c2f31c465dae4c6906db3269f
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-26 22:45:55 -0500

    rollback

--- edit.php
+++ edit.php
@@ -13,17 +13,7 @@ $local_username=$_SESSION['email'];
 
     <div class="card card-body">
 				
-			<div class="form-group">
-					<hr>
-					<center><label for="useful" style="color: black;"><strong>Menu de Opciones</strong></label></center><br>	
-					<hr>
-					<center><input type="submit" class="btn btn-success btn-block" name="logout" value="Delete" formaction="delete.php"></input></center><br><br><br>
-					<center><input type="submit" class="btn btn-success btn-block" name="logout" value="Cancel" formaction="videolinkadminmodule.php"></input></center>
-                    </div>
-			</div>
-        <!-- -->
-
-		<div class="col-md-4">
+			<div class="col-md-4">
 		     		 
 			 <?php 
 					$query = "select * from videotips_videotips where id = '$id' and username='$local_username'";
@@ -58,6 +48,11 @@ $local_username=$_SESSION['email'];
 						<select name="active" required><?php $query_options = "SELECT * FROM videotips_active"; $result_options = mysqli_query($conn, $query_options); while ($option = mysqli_fetch_assoc($result_options)) { $selected = ($option['active'] == $link['active']) ? "selected" : ""; echo "<option value=\"{$option['active']}\" $selected>{$option['active']}</option>"; } ?></select><br><br>
 					</div>
 					<center><input type="submit" class="btn btn-success btn-block" name="update_link" value="Update Link"></input></center><br><br><br>
+					<hr>
+					<center><label for="useful" style="color: black;"><strong>Menu de Opciones</strong></label></center><br>	
+					<hr>
+					<center><input type="submit" class="btn btn-success btn-block" name="logout" value="Delete" formaction="delete.php"></input></center><br><br><br>
+					<center><input type="submit" class="btn btn-success btn-block" name="logout" value="Cancel" formaction="videolinkadminmodule.php"></input></center>
 				</form>
 			</div>
 		</div>
