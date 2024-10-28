644bb1b8d6a654605645daf62c16aed842b0656b
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-28 14:20:37 -0500

    p

--- savelinks.php
+++ savelinks.php
@@ -19,29 +19,28 @@ $urlduplicated= $conn ->query($query1);
 
 
 if($urlduplicated->num_rows > 0){ 
-  echo ("Enlace duplicado, usar otro");
-  header("refresh:3; url=videolinkadminmodule.php");
-  exit();
-} else {
-if($activesuscription == 1){
-$query3="INSERT INTO videotips_videotips (videolink,maincategory,category,description,active,username) values ('$videolink','$maincategory','$category','$description','Yes','$local_username')";
-$resultado= $conn ->query($query3);
-$query4="INSERT INTO videotips_app_access_list (categorycounter) values ('categorycounter++')";
-$resultado1= $conn ->query($query4);
-  echo ("Enlace Salvado Exitosamente");
-  header("refresh:3; url=videolinkadminmodule.php");
-  exit();
-}
-
-if ($resultado){
-  echo ("Enlace Salvado Exitosamente");
-  header("refresh:3; url=videolinkadminmodule.php");
-  exit();
-  }
-else{
-  echo ("Suscripción vencida, por favor renovarla para continuar disfrutando de su biblioteca de sus enlaces favoritos");
-  header("refresh:3; url=videolinkadminmodule.php");
-  exit();
-}
-}
+                              echo ("Enlace duplicado, usar otro");
+                              header("refresh:3; url=videolinkadminmodule.php");
+                              exit();
+                              } else {
+                                    if($activesuscription == 1){
+                                                          $query3="INSERT INTO videotips_videotips (videolink,maincategory,category,description,active,username) values ('$videolink','$maincategory','$category','$description','Yes','$local_username')";
+                                                          $resultado= $conn ->query($query3);
+                                                          $query4="INSERT INTO videotips_app_access_list (categorycounter) values ('categorycounter++')";
+                                                          $resultado1= $conn ->query($query4);
+                                                          echo ("Enlace Salvado Exitosamente");
+                                                          header("refresh:3; url=videolinkadminmodule.php");
+                                                          exit();                           
+                                    if ($resultado){
+                                      echo ("Enlace Salvado Exitosamente");
+                                      header("refresh:3; url=videolinkadminmodule.php");
+                                      exit();
+                                      }
+                                    else{
+                                      echo ("Suscripción vencida, por favor renovarla para continuar disfrutando de su biblioteca de sus enlaces favoritos");
+                                      header("refresh:3; url=videolinkadminmodule.php");
+                                      exit();
+                                    }
+                                                                }
+                                    }
 ?>
