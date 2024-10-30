e3180368c6cff94a07f4e347ad7d6b2af4f5a223
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-30 12:10:49 -0500

    categorycounter

--- deletecategory.php
+++ deletecategory.php
@@ -3,8 +3,13 @@ include "db_connection1.php";
 $id=$_POST['id'];
 $local_username=$_SESSION['email'];
 
+
 $query = "delete from videotips_viodetipscategory where id='$id'";
 $result = mysqli_query($conn,$query);
+
+$query1 = "UPDATE videotips_app_access_list SET categorycounter = categorycounter - 1 WHERE username = '$local_username'";
+$result1 = mysqli_query($conn,$query1);
+
 if (!$result) {
     /*die("Category not found");*/
     die("Categoría no encontrada");
