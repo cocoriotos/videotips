89ce55fe5327f97cd741f346fc732bd3d4b37946
Julian GOnzalez <cocoriotos@hotmail.com>
2024-10-30 12:41:44 -0500

    ok

--- deletecategory.php
+++ deletecategory.php
@@ -3,8 +3,6 @@ include "db_connection1.php";
 $id=$_POST['id'];
 $local_username=$_SESSION['email'];
 
-$query1 = "UPDATE videotips_app_access_list SET categorycounter = categorycounter - 1 WHERE email = '$local_username'";
-$result1 = mysqli_query($conn,$query1);
 $query = "delete from videotips_viodetipscategory where id='$id'";
 $result = mysqli_query($conn,$query);
 
