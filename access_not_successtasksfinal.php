3a0dfc89d302a9a0dddd7ef0f94b8b4cecbc039e
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-01 09:22:50 -0500

    ok

--- access_not_successtasksfinal.php
+++ access_not_successtasksfinal.php
@@ -1,5 +1,6 @@
 <!--  Developed by julián González Bucheli -->
 <html lang="us"> 
+		  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 		  <script type="text/javascript">
 			/*alert("ALERT: Your Username is incorrect, please try again or select option butom to request access");*/
 			/*alert("ALERTA: Su Usuario es incorrecto, por favor trate nuevamente o seleccione la opción de requerir acceso");*/
