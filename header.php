add22fc1c26d974b5645b7c2bf60eca3712e6d26
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-02 15:05:35 -0500

    center

--- header.php
+++ header.php
@@ -9,15 +9,16 @@ include "nobackpage.php";
     <center><a href="videolinkadminmodule.php" class="navbar-brand">Bienvenido a tu Biblioteca de Enlaces Útiles</a></center>
     </div>
     <br>
-        <center><div class="text-center">
+  </nav>
+
+  <nav class="navbar navbar-dark bg-dark">
+	        <center><div class="text-center">
             <a href="videolinkadminmodule.php" class="btn" style="background-color: #FFF9CC; color: black; font-weight: bold;"><i class="fas fa-broom"></i> Limpiar Formulario</a>
             <a href="addcategory.php" class="btn" style="background-color: #D6EEFF; color: black; font-weight: bold;"><i class="fas fa-layer-group"></i> Categorías</a>
             <a href="videotrackerauth.php" class="btn btn-danger" style="background-color: #FFD6D6; color: black; font-weight: bold;"><i class="fas fa-sign-out-alt"></i> Salir</a>
-        </div>
-        </center>
-    <!--<center><a href="videolinkadminmodule.php" class="navbar-brand">Welcome To Your  Usefull Video Links</a></center>-->
-	
+            </div></center>	
   </nav>
+
   <!-- BOOTSTRAP -->	
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
 <!-- FONT AWESOME https://fontawesome.com/start and donwload the icons -->
