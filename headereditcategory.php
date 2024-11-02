8a754ac481d4ba49849b8b27138b90b368a0de96
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-02 00:23:54 -0500

    reply

--- headereditcategory.php
+++ headereditcategory.php
@@ -4,7 +4,7 @@
   <nav class="navbar navbar-dark bg-dark">
 	<div class="container">
     <center><a href="videolinkadminmodule.php" class="navbar-brand">Bienvenido a tu Biblioteca de Enlaces Útiles</a></center>
-    <a href="videolinkadminmodule.php" class="btn" style="background-color: #FFF9CC; color: black; font-weight: bold;"><i class="fas fa-broom"></i> Ir a adicionar enlaces</a>
+    <a href="videolinkadminmodule.php" class="btn" style="background-color: #FFF9CC; color: black; font-weight: bold;"><i class="fas fa-reply"></i> Ir a adicionar enlaces</a>
     <a href="addcategory.php" class="btn" style="background-color: #D6EEFF; color: black; font-weight: bold;"><i class="fas fa-reply"></i> Ir a Categorías</a>
     <a href="videotrackerauth.php" class="btn btn-danger" style="background-color: #FFD6D6; color: black; font-weight: bold;"><i class="fas fa-sign-out-alt"></i> Salir</a>
        
