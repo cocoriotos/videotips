59ec02ff284689383a10a057b1d0f6be9ae75d61
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-02 15:25:18 -0500

    release div

--- header.php
+++ header.php
@@ -5,9 +5,9 @@ include "nobackpage.php";
 <html lang="us"> <!-- Page language-->
 <header>
   <nav class="navbar navbar-dark bg-dark d-flex justify-content-center">
-	<div class="container">
+	
     <center><a href="videolinkadminmodule.php" class="navbar-brand">Bienvenido a tu Biblioteca de Enlaces Útiles</a></center>
-    </div>
+    
   </nav>
 
   <center><nav class="navbar navbar-dark bg-dark d-flex justify-content-center align-items-center">
