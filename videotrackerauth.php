59c58cac767ab225d1d65732a9b6567e98477e5d
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-02 21:35:14 -0500

    hum

--- videotrackerauth.php
+++ videotrackerauth.php
@@ -18,7 +18,7 @@
 			<div class="information">
 				<div class="info-childs">
 					<h2>Bienvenido</h2>
-					<p>La intención de esta herramienta es que tengas organizados tus enlaces favoritos de calquier página en un solo sitio donde los deberá categorizar y etiquetar para que la busqueda sea muy rpaida y así evitar el buscarlos en los histíricos de las otras aplicaciones c</p>
+					<p>La intención de esta herramienta es que tengas organizados tus enlaces favoritos de calquier página en un solo sitio donde los deberá categorizar y etiquetar para que la busqueda sea muy rpaida y así evitar el buscarlos en los histíricos de las otras aplicaciones.</p>
 					<input type="button" value="Iniciar Sesión"></input>
 				</div>
 			</div>
