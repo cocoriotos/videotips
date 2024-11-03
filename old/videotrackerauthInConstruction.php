0a94a58288c6e50ead0e7e560325179c453dec62
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-02 21:57:51 -0500

    rollbackauthform

--- old/videotrackerauthInConstruction.php
+++ old/videotrackerauthInConstruction.php
@@ -1,28 +1,47 @@
 <!--  Developed by julián González Bucheli -->
 <html lang="us"> <!-- Page language-->
-	<?php include "nobackpage.php"; include "SessionTimeOut.php";?>
+	<?php 
+	include "nobackpage.php"; 
+	include "SessionTimeOut.php";
+	?>
 	<head>	
-	<script src="head.js" defer></script>		  
+	<script src="headlogin.js" defer></script>	  
 	</head>
-	<header>
+	<!--  <header>
 		<!--<HR id="HR"/>	<!-- Header title  --> <!-- -->
-		<br>
+		<!--  <br>
 			<center><h1><font id="form_title" color="#E1EAF7">Herramienta de Enlaces Útiles</font></h1></center><br><br>
 		<!--<HR/>-->
-	</header>	
-	  <body id="bodyadminmodule">
+	<!--</header>-->
+	<body>
+		><div class="container-form">
+			<div class="information">
+				<div class="info-childs">
+					<h2>Bienvenido</h2>
+					<p>La intención de esta herramienta es que tengas organizados tus enlaces favoritos de calquier página en un solo sitio donde los deberá categorizar y etiquetar para que la busqueda sea muy rpaida y así evitar el buscarlos en los interminables enlaces en históricos de las aplicaciones.</p>
+					<input type="button" value="Iniciar Sesión"></input>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+
+
+
+
+	<!--<body id="bodyadminmodule">
 		<form id="login" action="access_success_Tasks_final.php" method="POST" autocomplete="off"> <!-- Form to login into application with authentication in database and valid username -->
-			    <center>
+		    <!--<center>
 					<font color=lightblue id="form_title"><strong>Formulario de Autenticación</strong></font><br><br>
                 	<img id="img_login" center SRC="login.gif"></img></br> <!-- Login Icon  -->
-					<div class="inputdata1">
+					<!--<div class="inputdata1">
 						<font id= "form_title" color="white"><strong>Email</strong></font><br><br>
 						<input id="username1" type="text" name="email"  placeholder="Digite el email" required ><br> <!-- Login  --><br>
-					</div >
+					<!--</div >
 					<div class="inputdata1">
 						<font id= "form_title" color="white"><strong>Contraseña</strong></font><br><br>
 						<input id="username1" type="password" name="password" placeholder="Digite su Constraseña" required ><br> <!-- Login PAM username  --><br>
-					</div >
+					<!--</div >
 					<input id="loginbutton" type="submit" value="Ingresar">
 					<br>
 					<br>
@@ -30,7 +49,7 @@
 				</center>
 		</form>	
 		<form id="login" action="requestaccessfinal.php" method="POST" autocomplete="off"> <!-- Form to request access-->
-			<center>
+			<!--<center>
 				<h6 id="access" align="center"><address><strong>Sin acceso?, Por favor solicitarlo acá</strong> </address></h6><br>
 				<input id="requestaccess" type="submit" value="Solicitar Acceso"><br>
 			</center>
@@ -48,9 +67,9 @@
 					<h6 align="center"><address><strong>Email:</strong>cocoriotos@hotmail.com</address></h6>
 					<h6 align="center"><address><strong>Phone:</strong>(+57) 305 429 31 85 </address></h6>
 					<h6 align="center"><address><strong>Pereira - Colombia </strong></address></h6>
-					<h6 align="center" datetime="<?php echo date('Y-m-d H:i'); ?>" pubdate> Fecha: <?php echo date('m/d/Y'); ?> Hora: <?php echo date('H:i'); ?></h6>
+					<h6 align="center" datetime="<?php /*echo date('Y-m-d H:i'); ?>" pubdate> Fecha: <?php echo date('m/d/Y'); ?> Hora: <?php echo date('H:i');*/ ?></h6>
 					<!--<h6 time align="center" datetime="2024/8/30" pubdate>Published on Date: 30/08/2024 - WEB Page available: 7*24</time></h6> <!-- Publication format layout-->
-				</hgroup>
+		<!--		</hgroup>
 		</footer>
 	</body>
 </html>
