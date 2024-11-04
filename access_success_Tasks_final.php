068f81f9056a0fc30c6640f24923989fe1295db6
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-04 03:10:15 -0500

    left

--- access_success_Tasks_final.php
+++ access_success_Tasks_final.php
@@ -49,7 +49,8 @@ $password=$_POST['password'];
 				$query1="select * from videotips_app_access_list where email='$local_username' and active='1' and password='$password'"; 
 				$result1=mysqli_query($conn, $query1); 
 
-				if ($suscriptiondaysleft->num_rows > 8    && $suscriptionpayed->num_rows < 1 ) {
+				if ($suscriptiondaysleft > 8) 
+				/*&& $suscriptionpayed < 1)*/ {
 					include("FreeSuscriotionReached.php");
 					header("refresh:0; url=suscriptionpayment.php");
 					exit();
