cd397573db8531b1c930d60e957dd1a34015f2c9
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-03 22:47:02 -0500

    paymentnequi css

---
+++ headersuscription.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<?php
+include "nobackpage.php";
+?>
+<html lang="us"> <!-- Page language-->
+<header>
+  <nav class="navbar navbar-dark bg-dark d-flex justify-content-center" id="welcome">
+	    <center><a id="welcome" href="videolinkadminmodule.php" class="navbar-brand">Bienvenido a tu Biblioteca de Enlaces Útiles</a></center>
+  </nav>
+
+  <center><nav class="navbar navbar-dark bg-dark d-flex justify-content-center align-items-center">
+	        <div class="text-center">
+                <a onclick="window.open('https://clientes.nequi.com.co/recargas', '_blank');" class="btn btn-success btn-block" ><i class="fas fa-dollar-sign"></i>Pago por Nequi</ar><br><br>
+                <a href="videotrackerauth.php" class="btn btn-danger" style="background-color: #FFD6D6; color: black; font-weight: bold;"><i class="fas fa-sign-out-alt"></i> Salir</a>
+            </div>	
+  </nav></center>
+
+  <!-- BOOTSTRAP -->	
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
+<!-- FONT AWESOME https://fontawesome.com/start and donwload the icons -->
+    <script src="https://kit.fontawesome.com/60f0db780e.js" crossorigin="anonymous"></script>
+	</header>
+    </html>
