b85507b9d2d4d0ff631423863c30de061ce63039
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-03 19:52:34 -0500

    diffdays

--- access_success_Tasks_final.php
+++ access_success_Tasks_final.php
@@ -18,30 +18,38 @@ $password=$_POST['password'];
   $db_pass="C0mp13t3501ut10n5*";
   $db_name="u927778197_appsdb";
   $conn=mysqli_connect($db_host,$db_user,$db_pass,$db_name);
-  if(mysqli_connect_errno()) 
-			{	
-			include("No_DB_Connectionfinal.php");
-			include ("videotrackerauth.php");
-			exit();
-			}
-  mysqli_select_db($conn,$db_name) or die ("<center>No hay conexión disponible a la aplicación</center>");		
+		if(mysqli_connect_errno()) 
+					{	
+					include("No_DB_Connectionfinal.php");
+					include ("videotrackerauth.php");
+					exit();
+					}
+		mysqli_select_db($conn,$db_name) or die ("<center>No hay conexión disponible a la aplicación</center>");		
 
-  if ($conn==true)
-		{
+		if ($conn==true)
+				{
 
-		}
-		$query1="select * from videotips_app_access_list where email='$local_username' and active='1' and password='$password'"; /*if username*/
-		$result1=mysqli_query($conn, $query1);
-	
-		if(mysqli_num_rows($result1)==true)
-			{	
-			include("videolinkadminmodule.php");
-			exit();	
-			}
-		else 
-		{
-		include ("access_not_successtasksfinal.php");
-		}		
+				}
+				
+				$query2="update videotips_app_access_list SET suscriptiondaysleft = DATEDIFF(CURDATE(), registrationdate) where email='$local_username'"; 
+				$result2=mysqli_query($conn, $query2);
+
+
+
+				$query1="select * from videotips_app_access_list where email='$local_username' and active='1' and password='$password'"; 
+				$result1=mysqli_query($conn, $query1);
+
+				
+			
+				if(mysqli_num_rows($result1)==true)
+					{	
+					include("videolinkadminmodule.php");
+					exit();	
+					}
+				else 
+				{
+				include ("access_not_successtasksfinal.php");
+				}		
  }
 ?>	
 </html>
