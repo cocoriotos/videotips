b956de0f96ae241cc15a23aa92f0b9bbd491280e
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-03 23:31:36 -0500

    container

--- headersuscription.php
+++ headersuscription.php
@@ -19,7 +19,7 @@ include "nobackpage.php";
           <div class="form-group">			
             <center><input type="button" class="btn btn-success btn-block" value="Pago por Nequi" onclick="window.open('https://clientes.nequi.com.co/recargas', '_blank');"></center><br><br>
           </div>
-
+  <div class="container">
       <div class="column-wrap clearfix">
             <div class="col-xs-12">
                 <hr>
@@ -36,7 +36,8 @@ include "nobackpage.php";
 				        </div>
           </div>
       </div>    
-</body>
+  </div>
+    </body>
 
   <!-- BOOTSTRAP -->	
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
