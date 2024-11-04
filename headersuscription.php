7ade66e1b062e915ee7c36a5c15f335b2e9619be
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-04 01:29:36 -0500

    Payment

--- headersuscription.php
+++ headersuscription.php
@@ -26,15 +26,57 @@ include "nobackpage.php";
             </div>
           <div class="col-xs-12 col-sm-4 column-custom-wrap">
                 <div class="column-custom">
-                    <h3 align="center" style="padding-bottom: 5px">LIVRARIA</h3>
+                    <h3 align="center" style="padding-bottom: 5px">Condiciones y Beneficioes de Uso de la Aplicación</h3>
+                    <!--<p> Video paso a paso del uso de la aplicación. Click Aquí</p><br>-->
                     <br>
-                    <p>Librería virtual que busca mantener la tradición de la lectura física, apostando principalmente a las segundas oportunidades de libros leídos, además de buscar por ti los títulos que quieres. Encontrarás en el catálogo: literatura Colombiana, filosofía, poesía, literatura universal, ciencias sociales, psicología e historia.</p>                   
-				            <br>
+                    <center><p style="color: blue; font-weight: bold;"> Ventajes </p></center>
+                    <p> Gracias por haber usado gratuitamente por 7 días la biblioteca de Enlaces útiles la cuál te ayuda a: </p><br>
+                    <p> 1. Crear gratuitamente hasta 5 Subcategoorías para organizar tus enlaces</p>  
+                    <p> 2. Etiquetar con una breve descripción del contenido para que puedas hacer las búsquedas más facilmente</p>
+                    <p> 3. Tener todos tus enlaces importantes indistinto de cual plataforma sea y que la búsqueda la haces en un solo sitio</p>
+                    <p> 4. La búsqueda de un Enlaces que normalmente lleva mucho timepo encontrarla, cuando tienes muchos contenidos grabados en la plataforma origen en los favoritos o hisstoricos, la harás en segundos</p>
+                    <p> 5. Abrir el enlace directamente para ver los contenidos</p>
+                    <p> 6. Copiar los Enlaces guardados para compartirlos</p>
 					          <br>
-					          <a href="/LIVRARIA/index.php" rel="nofollow">* Consulta los libros disponibles y llena el formulario para solicitarlo</a>			
-					          <br>
+                    <center><p style="color: blue; font-weight: bold;"> Condiciones </p></center>
+                    <p> Para tener en cuenta: </p><br>
+                    <p> 1. No incluir información sensitiva ni personales. No nos hacemos responsables del salvaguardar o uso de su información</p>
+                    <p> 2. Tratamiento de los datos bajo la modalidad de Abeas Data</p>
+                    <p> 3. El valor de la Suscripción es anual. 60.000 Pesos Colombianos COP</p>
+                    <p> 4. El valor de la habilitación de Subcategorías más allá de 5 es vitalicia. 15.000 Pesos Colombianos COP</p>
+                    <br>
+                    <center><p style="color: blue; font-weight: bold;"> Forma de Pago de Suscripción y de Subcategorías</p></center>
+                    <p> 1. Pago de los montos por Nequi al número +57 305 4293185.</p> 
+                    <p> 2. Tomar una imagen por cada uno de los pagos realizados y convertirlas a formato PDF</p>
+                    <p> 3. Al realizar el o los pagos llenar el formulario por casa tipo de producto adquirido incluyendo el nombre de usuario con el que se registró, recuerde que es su cuenta de correo.</p>
+                    <p> 4. En el formulario al registrar los pagos indicar por separado si es de suscripción o de subcategorías</p>
+                    <p> 5. El pago de categorías solo debe hacerse si tiene una suscripción activa para que sea efectivo</p>
+                    <p> 6. Si tiene alguna duda, enviar correo al adminitrador: cocoriotos@hotmail.com"o WhatsApp: +57 3054293185</p>
 				        </div>
           </div>
+          <hr>
+          <form id="login" action="suscriptionpaymentevidence.php" method="POST" autocomplete="off"> <!-- Form to login into application with authentication in database and valid username -->
+              <center>
+                  <font color=lightblue id="form_title"><strong>Formulario de Evidencia de Pago de Productos</strong></font><br><br>
+                          <img id="img_login" center SRC="login.gif"></img></br> <!-- Login Icon  -->
+                  <div class="inputdata1">
+                    <font id= "form_title" color="white"><strong>Email</strong></font><br><br>
+                    <input id="username1" type="text" name="email"  placeholder="Digite el email" required ><br> <!-- Login  --><br>
+                  </div >
+                  <div class="inputdata1">
+                    <font id= "form_title" color="white"><strong>Producto adquirido</strong></font><br><br>
+                    <input id="username1" type="text" name="product" placeholder="Seleccione su producto" required ><br> <!-- Login PAM username  --><br>
+                  </div >
+                  <div class="inputdata1">
+                    <font id= "form_title" color="white"><strong>Cargar imagen en formato PDF</strong></font><br><br>
+                    <input id="username1" type="text" name="product" placeholder="Seleccione su producto" required ><br> <!-- Login PAM username  --><br>
+                  </div >
+                  <input id="loginbutton" type="submit" value="Enviar">
+                  <br>
+                  <br>
+                  <br>
+				      </center>
+		     </form>
       </div>    
   </div>
     </body>
