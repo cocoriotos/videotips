039c2cfd65c7de38330504c13296a0d4745da31b
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-05 12:12:56 -0500

    get categories

---
+++ get_subcategories.php
@@ -0,0 +1,16 @@
+<?php
+include 'conexion.php'; // Incluye tu conexión a la base de datos
+
+if (isset($_POST['maincategory'])) {
+    $maincategory = $_POST['maincategory'];
+    $username = $local_username; // Asegúrate de definir $local_username correctamente
+
+    $SQLSELECT = "SELECT DISTINCT(category) FROM videotips_viodetipscategory WHERE maincategory = '$maincategory' AND username = '$username' ORDER BY category ASC";
+    $result_set = mysqli_query($conn, $SQLSELECT);
+
+    while ($rows = $result_set->fetch_assoc()) {
+        $category = $rows['category'];
+        echo "<option value='$category'>$category</option>";
+    }
+}
+?>
