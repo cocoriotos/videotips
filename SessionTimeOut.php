6ca4befc5c7c7e2c92f7819f3c4c46140d89f0a8
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-04 20:54:27 -0500

    g

--- SessionTimeOut.php
+++ SessionTimeOut.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['LAST_ACTIVITY'])) {
     if ($elapsed_time > $timeout_duration) {
         // Si el tiempo de inactividad excede el límite, cerrar la sesión
 		include("closetaskscon.php");
-        header("Location: videotrackerauth.php"); // Redirige a la página de autenticación
+        header("refresh:0; url=videotrackerauth.php"); // Redirige a la página de autenticación
         exit();
     }
 }
