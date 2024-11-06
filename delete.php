5128a6d5166344108939eea67e73ac20a3a7873d
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-06 18:38:07 -0500

    delete

--- delete.php
+++ delete.php
@@ -3,7 +3,8 @@ include "db_connection1.php";
 $id=$_POST['id'];
 $local_username=$_SESSION['username'];
 
-$query = "update videotips_videotips SET active = 'No' and uername = '$local_username' where id='$id'";
+/*$query = "update videotips_videotips SET active = 'No' and username = '$local_username' where id='$id'";*/
+$query = "delete from videotips_videotips where id='$id'";
 $result = mysqli_query($conn,$query);
 if (!$result) {
     /*die("Link not found");*/
