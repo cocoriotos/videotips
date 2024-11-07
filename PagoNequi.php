dda5c204a887606562611a2730c77bbfecb80f2a
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-06 23:03:07 -0500

    PN

---
+++ PagoNequi.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<?php
+include "nobackpage.php";
+/*include "SessionTimeOut.php";*/
+
+?>
+<html lang="us"> <!-- Page language-->
+<header>
+  <nav class="navbar navbar-dark bg-dark d-flex justify-content-center" id="welcome">
+	    <center><a id="welcome" href="videolinkadminmodule.php" class="navbar-brand">Consideraciones para el Pago por Nequi y cubrimientos de Servicios</a></center>
+  </nav>
+
+  <center><nav class="navbar navbar-dark bg-dark d-flex justify-content-center align-items-center">
+	        <div class="text-center">
+                <a href="videotrackerauth.php" class="btn btn-danger" style="background-color: #FFD6D6; color: black; font-weight: bold;"><i class="fas fa-sign-out-alt"></i> Salir</a>
+            </div>	
+  </nav></center>
+  <script src="copynumber.js"></script>
+  	
+<body id="bodyadminmodule">
+          <br><br>
+          <div class="form-group">			
+            <center>
+              <input type="button" class="btn btn-success btn-block" value="Número Nequi: 3054293185" onclick="copiarNumero();">  
+              <input type="button" class="btn btn-success btn-block" value="Pago con número Telefónico por Nequi" onclick="window.open('https://clientes.nequi.com.co/recargas', '_blank');"></input>
+            </center><br><br>
+          </div>
+  <div class="container">
+      <div class="column-wrap clearfix">
+            <div class="col-xs-12">
+                <hr>
+            </div>
+      <center><div class="col-xs-12 col-sm-4 column-custom-wrap">
+                <div class="column-custom">
+                    <h3 align="center" style="padding-bottom: 5px">Condiciones y Beneficioes de Uso de la Aplicación</h3>
+                    <!--<p> Video paso a paso del uso de la aplicación. Click Aquí</p><br>-->
+                    <br>
+                    <center><p style="color: blue; font-weight: bold;"> Ventajas </p></center>
+                    <p> Gracias por haber usado gratuitamente por 7 días la biblioteca de Enlaces útiles la cuál te ayuda a: </p><br>
+                    <p> 1. Crear gratuitamente hasta 5 Subcategoorías para organizar tus enlaces</p>  
+                    <p> 2. Etiquetar con una breve descripción del contenido para que puedas hacer las búsquedas más facilmente</p>
+                    <p> 3. Tener todos tus enlaces importantes indistinto de cual plataforma sea y que la búsqueda la haces en un solo sitio</p>
+                    <p> 4. La búsqueda de un Enlaces que normalmente lleva mucho timepo encontrarla, cuando tienes muchos contenidos grabados en la plataforma origen en los favoritos o hisstoricos, la harás en segundos</p>
+                    <p> 5. Abrir el enlace directamente para ver los contenidos</p>
+                    <p> 6. Copiar los Enlaces guardados para compartirlos</p>
+					          <br>
+                    <center><p style="color: blue; font-weight: bold;"> Condiciones </p></center>
+                    <p> Para tener en cuenta: </p><br>
+                    <p> 1. No incluir información sensitiva ni personales. No nos hacemos responsables del salvaguardar o uso de su información</p>
+                    <p> 2. Tratamiento de los datos bajo la modalidad de Abeas Data</p>
+                    <p> 3. El valor de la Suscripción es anual. 60.000 Pesos Colombianos COP</p>
+                    <p> 4. El valor de la habilitación de Subcategorías más allá de 5 es vitalicia. 15.000 Pesos Colombianos COP</p>
+                    <br>
+                    <center><p style="color: blue; font-weight: bold;"> Forma de Pago de Suscripción y de Subcategorías</p></center>
+                    <p> 1. Pago de los montos por Nequi al número +57 305 4293185.</p> 
+                    <p> 2. Tomar una imagen por cada uno de los pagos realizados y convertirlas a formato PDF</p>
+                    <p> 3. Al realizar el o los pagos llenar el formulario por casa tipo de producto adquirido incluyendo el nombre de usuario con el que se registró, recuerde que es su cuenta de correo.</p>
+                    <p> 4. En el formulario al registrar los pagos indicar por separado si es de suscripción o de subcategorías</p>
+                    <p> 5. El pago de categorías solo debe hacerse si tiene una suscripción activa para que sea efectivo</p>
+                    <p> 6. Si tiene alguna duda, enviar correo al adminitrador: cocoriotos@hotmail.com" o WhatsApp: +57 3054293185</p>
+				        </div>
+          </div>
+          </center>
+          <hr>
+          <!--<form id="login" action="suscriptionpaymentevidence.php" method="POST" autocomplete="off"> <!-- Form to login into application with authentication in database and valid username -->
+              <!--<center>
+                  <font color=lightblue id="form_title"><strong>Formulario de Evidencia de Pago de Productos</strong></font><br><br>
+                          <img id="img_login" center SRC="login.gif"></img></br> <!-- Login Icon  -->
+                  <!--<div class="inputdata1">
+                    <font id= "form_title" color="white"><strong>Email</strong></font><br><br>
+                    <input id="username1" type="text" name="email"  placeholder="Digite el email" required ><br> <!-- Login  --><br>
+                  <!--</div >
+                  <div class="inputdata1">
+                    <font id= "form_title" color="white"><strong>Producto adquirido</strong></font><br><br>
+                    <input id="username1" type="text" name="product" placeholder="Seleccione su producto" required ><br> <!-- Login PAM username  --><br>
+                  <!--</div >
+                  <div class="inputdata1">
+                    <font id= "form_title" color="white"><strong>Cargar imagen en formato PDF</strong></font><br><br>
+                    <input id="username1" type="text" name="product" placeholder="Cargar el documento de evidencia" required ><br> <!-- Login PAM username  --><br>
+                  <!--</div >
+                  <input id="loginbutton" type="submit" value="Enviar">
+                  <br>
+                  <br>
+                  <br>
+				      </center>
+		     </form>-->
+
+         <form id="login" action="suscriptionpaymentevidence.php" method="POST" autocomplete="off">
+  <center>
+    <font color="lightblue" id="form_title"><strong>Formulario de Evidencia de Pago de Productos</strong></font><br><br>
+    <img id="img_login" src="login.gif" alt="Login Icon"><br>
+
+    <div class="inputdata1">
+      <font id="form_title" color="white"><strong>Email</strong></font><br><br>
+      <input id="username1" type="text" name="email" placeholder="Digite el email" required><br><br>
+    </div>
+
+    <div class="inputdata1">
+      <font id="form_title" color="white"><strong>Producto adquirido</strong></font><br><br>
+      <input id="username1" type="text" name="product" placeholder="Seleccione su producto" required><br><br>
+    </div>
+
+    <div class="inputdata1">
+      <font id="form_title" color="white"><strong>Cargar imagen en formato PDF</strong></font><br><br>
+      <input id="username1" type="text" name="product" placeholder="Cargar el documento de evidencia" required><br><br>
+    </div>
+
+    <!-- Checkbox de términos y condiciones -->
+    <div class="inputdata1">
+      <input type="checkbox" id="terms" onclick="toggleSubmitButton()"> Acepto los <a href="terminos.html" target="_blank">términos y condiciones</a><br><br>
+    </div>
+
+    <!-- Botón de enviar, inicialmente deshabilitado -->
+    <input id="loginbutton" type="submit" value="Enviar" disabled><br><br><br>
+  </center>
+</form>
+
+<script>
+  // Función para habilitar o deshabilitar el botón de enviar
+  function toggleSubmitButton() {
+    const submitButton = document.getElementById("loginbutton");
+    const termsCheckbox = document.getElementById("terms");
+
+    // Habilita el botón si el checkbox está marcado, de lo contrario lo deshabilita
+    submitButton.disabled = !termsCheckbox.checked;
+  }
+</script>
+
+
+
+      </div>    
+  </div>
+    </body>
+
+  <!-- BOOTSTRAP -->	
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
+<!-- FONT AWESOME https://fontawesome.com/start and donwload the icons -->
+    <script src="https://kit.fontawesome.com/60f0db780e.js" crossorigin="anonymous"></script>
+	</header>
+    </html>
