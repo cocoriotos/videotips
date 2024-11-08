526f2d1921448f29e8b3620ab5a9f80e12506d57
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-08 18:37:18 -0500

    T&C

--- TermsConditions.php
+++ TermsConditions.php
@@ -2,8 +2,10 @@
 <?php
 include "nobackpage.php";
 /*include "SessionTimeOut.php";*/
-
 ?>
+<head>
+<link rel="stylesheet" href="style_sheet.css"></link>
+</head>
 <html lang="us"> <!-- Page language-->
 <header>
   <nav class="navbar navbar-dark bg-dark d-flex justify-content-center" id="welcome">
