605871f6ec0828ad67da147ff337cab42466706a
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-08 22:48:09 -0500

    hs

--- headersuscription.php
+++ headersuscription.php
@@ -41,26 +41,26 @@ include "db_connection1.php";
                     <h3 align="center" style="padding-bottom: 5px">Condiciones y Beneficioes de Uso de la Aplicación</h3>
                     <!--<p> Video paso a paso del uso de la aplicación. Click Aquí</p><br>-->
                     <br>
-                    <center><p style="color: blue; font-weight: bold;"> Ventajas </p></center>
+                    <center><p style="color: blue; font-weight: bold; font-size: 24px;"> Ventajas </p></center>
                     <p> Gracias por haber usado gratuitamente por 7 días la biblioteca de Enlaces útiles la cuál te ayuda a: </p><br>
                     <p> 1. Crear gratuitamente hasta 5 Subcategoorías para organizar tus enlaces</p>  
                     <p> 2. Etiquetar con una breve descripción del contenido para que puedas hacer las búsquedas más facilmente</p>
                     <p> 3. Tener todos tus enlaces importantes indistinto de cual plataforma sea y que la búsqueda la haces en un solo sitio</p>
-                    <p> 4. La búsqueda de un Enlaces que normalmente lleva mucho timepo encontrarla, cuando tienes muchos contenidos grabados en la plataforma origen en los favoritos o hisstoricos, la harás en segundos</p>
+                    <p> 4. La búsqueda de un enlaces en cualquier histórico o favoritos en las aplicaciones, que normalmente toma mucho tiempo encontrarla, la harás en segundos porque las tendrás en un solo lugar y con un motor de búsqueda efectivo</p>
                     <p> 5. Abrir el enlace directamente para ver los contenidos</p>
                     <p> 6. Copiar los Enlaces guardados para compartirlos</p>
 					          <br>
-                    <center><p style="color: blue; font-weight: bold;"> Condiciones </p></center>
+                    <center><p style="color: blue; font-weight: bold; font-size: 24px;"> Condiciones </p></center>
                     <p> Para tener en cuenta: </p><br>
                     <p> 1. No incluir información sensitiva ni personales. No nos hacemos responsables del salvaguardar o uso de su información</p>
                     <p> 2. Tratamiento de los datos bajo la modalidad de Abeas Data</p>
-                    <p> 3. El valor de la Suscripción es anual. 60.000 Pesos Colombianos COP</p>
-                    <p> 4. El valor de la habilitación de Subcategorías más allá de 5 es vitalicia. 15.000 Pesos Colombianos COP</p>
+                    <p> 3. El valor de la Suscripción es de $60.000 Pesos Colombianos COP Anuales</p>
+                    <p> 4. El valor de la habilitación de Subcategorías más allá de 5 es vitalicia. $15.000 Pesos Colombianos COP</p>
                     <br>
-                    <center><p style="color: blue; font-weight: bold;"> Forma de Pago de Suscripción y de Subcategorías</p></center>
+                    <center><p style="color: blue; font-weight: bold; font-size: 24px;"> Forma de Pago de Suscripción y Subcategorías</p></center>
                     <p> 1. Pago de los montos por Nequi al número +57 305 4293185.</p> 
                     <p> 2. Tomar una imagen por cada uno de los pagos realizados y convertirlas a formato PDF</p>
-                    <p> 3. Al realizar el o los pagos llenar el formulario por casa tipo de producto adquirido incluyendo el nombre de usuario con el que se registró, recuerde que es su cuenta de correo.</p>
+                    <p> 3. Al realizar el o los pagos llenar el formulario por cada tipo de producto adquirido incluyendo el nombre de usuario con el que se registró que es su cuenta de correo.</p>
                     <p> 4. En el formulario al registrar los pagos indicar por separado si es de suscripción o de subcategorías</p>
                     <p> 5. El pago de categorías solo debe hacerse si tiene una suscripción activa para que sea efectivo</p>
                     <p> 6. Si tiene alguna duda, enviar correo al adminitrador: cocoriotos@hotmail.com" o WhatsApp: +57 3054293185</p>
