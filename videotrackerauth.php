a274f74257a1c8e7dbfd6f050d44579ba4f1dfb3
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-10 10:24:58 -0500

    reponse

--- videotrackerauth.php
+++ videotrackerauth.php
@@ -7,7 +7,7 @@
 	<link>	
 	<script src="head.js" defer></script>	
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"></link>	  
-	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 	<header>
 			<HR id="HR"/>	<!-- Header title  --> <!-- -->
