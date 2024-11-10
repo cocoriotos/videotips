0f36239ee8b6aa202eb44066c8cb48eb63b3e94e
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-10 11:23:07 -0500

    ok

--- videotrackerauth.php
+++ videotrackerauth.php
@@ -41,7 +41,7 @@
 				<input id="requestaccess" type="submit" value="Solicitar Acceso"><br>
 			</center>
 		</form>
-		<center><footer id="contact">
+		<footer id="contact">
 					<hgroup>
 							<h6 align="center"><address><strong>Web Admin:</strong> Julian Gonzalez Bucheli</address></h6>
 							<h6 align="center"><address><strong>Email:</strong>cocoriotos@hotmail.com</address></h6>
@@ -50,6 +50,6 @@
 							<h6 align="center" datetime="<?php echo date('Y-m-d H:i'); ?>" pubdate> Fecha: <?php echo date('m/d/Y'); ?> Hora: <?php echo date('H:i'); ?></h6>
 							<!--<h6 time align="center" datetime="2024/8/30" pubdate>Published on Date: 30/08/2024 - WEB Page available: 7*24</time></h6> <!-- Publication format layout-->
 					</hgroup>
-		        </footer></center>
+		        </footer>
 	</body>
 </html>
