67d841d8d0ed0f1b92c7a68519290bf158f6cebd
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-10 12:17:46 -0500

    ok

--- videotrackerauth.php
+++ videotrackerauth.php
@@ -40,17 +40,17 @@
 				<input id="requestaccess" type="submit" value="Solicitar Acceso"><br>
 			</center>
 		</form>
-		<HR id="HR"/>
 		<footer id="contact">	
 					<hgroup>
+					<HR id="HR"/>
 							<h6 align="center"><address><strong>Web Admin:</strong> Julian Gonzalez Bucheli</address></h6>
 							<h6 align="center"><address><strong>Email:</strong>cocoriotos@hotmail.com</address></h6>
 							<h6 align="center"><address><strong>Phone:</strong>(+57) 305 429 31 85 </address></h6>
 							<h6 align="center"><address><strong>Pereira - Colombia </strong></address></h6>
 							<h6 align="center" datetime="<?php echo date('Y-m-d H:i'); ?>" pubdate> Fecha: <?php echo date('m/d/Y'); ?> Hora: <?php echo date('H:i'); ?></h6>
 							<!--<h6 time align="center" datetime="2024/8/30" pubdate>Published on Date: 30/08/2024 - WEB Page available: 7*24</time></h6> <!-- Publication format layout-->
+					<HR/>
 					</hgroup>			
 		</footer>
-		<HR/>
 	</body>
 </html>
