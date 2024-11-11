53d259e664a558943cf5a09a2ae1b74ddd16d4f1
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-10 19:45:26 -0500

    col instead row

--- videolinkadminmodule.php
+++ videolinkadminmodule.php
@@ -16,7 +16,7 @@ include "nobackpage.php";
 </head>
 <body id="bodyadminmodule">
 		<div  class="container p-4" >
-			<div class="row">
+			<div class="col">
 				<div class="col-md-4"> 
 					<div class="card card-body">
 						<form class="" action="savelinks.php" method="POST"> 
