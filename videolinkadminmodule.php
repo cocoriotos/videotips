57604319e5e3af991743c919d4720654f431719b
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-10 21:50:43 -0500

    hzok

--- videolinkadminmodule.php
+++ videolinkadminmodule.php
@@ -19,36 +19,55 @@ include "nobackpage.php";
 			<div class="row justify-content-start">
 				<div class="col-md-12"> 
 					<div class="card card-body">
-						<form class="" action="savelinks.php" method="POST"> 
-							<div class="form-group col">
-									<label for="videolink" class="col-md-2 col-form-label" style="color: black;"><strong>Enlace Útil</strong></label>	
-								<div class="col-md-10">
-									<textarea name="videolink" rows="5" class="form-control" placeholder="Enlace Útil"></textarea>
-								</div>
+					<form action="savelinks.php" method="POST">
+						<div class="row">
+							<!-- Enlace Útil -->
+							<div class="form-group col-md-3">
+								<label for="videolink" class="col-form-label" style="color: black;"><strong>Enlace Útil</strong></label>
+								<textarea name="videolink" rows="5" class="form-control" placeholder="Enlace Útil"></textarea>
 							</div>
 
-							<div class="form-group col">
-									<label for="maincategory" class="col-md-2 col-form-label" style="color: black;"><strong>Categoría</strong></label>
-								<div class="col-md-10">
-									<select class="form-control" name= "maincategory"> <?php $SQLSELECT = "SELECT distinct(maincategory) FROM videotips_viodetipscategory where username = '$local_username' order by maincategory asc "; $result_set = mysqli_query($conn, $SQLSELECT); while ($rows = $result_set ->fetch_assoc()) { $maincategory = $rows['maincategory']; echo "<option value='$maincategory'>$maincategory</option>";} ?></select>
-								</div>	
+							<!-- Categoría -->
+							<div class="form-group col-md-3">
+								<label for="maincategory" class="col-form-label" style="color: black;"><strong>Categoría</strong></label>
+								<select class="form-control" name="maincategory">
+									<?php 
+										$SQLSELECT = "SELECT distinct(maincategory) FROM videotips_viodetipscategory WHERE username = '$local_username' ORDER BY maincategory ASC"; 
+										$result_set = mysqli_query($conn, $SQLSELECT); 
+										while ($rows = $result_set->fetch_assoc()) { 
+											$maincategory = $rows['maincategory']; 
+											echo "<option value='$maincategory'>$maincategory</option>";
+										}
+									?>
+								</select>
 							</div>
-								
-							<div class="form-group col">
-									<label for="category" class="col-md-2 col-form-label" style="color: black;"><strong>Subcategoría</strong></label>
-								<div class="col-md-10">
-										<select class="form-control" name= "category"> <?php $SQLSELECT = "SELECT distinct(category) FROM videotips_viodetipscategory where username = '$local_username' order by category asc"; $result_set =  mysqli_query($conn, $SQLSELECT); while ($rows = $result_set ->fetch_assoc()) { $category = $rows['category']; echo "<option value='$category'>$category</option>";} ?></select>
-								</div>
+
+							<!-- Subcategoría -->
+							<div class="form-group col-md-3">
+								<label for="category" class="col-form-label" style="color: black;"><strong>Subcategoría</strong></label>
+								<select class="form-control" name="category">
+									<?php 
+										$SQLSELECT = "SELECT distinct(category) FROM videotips_viodetipscategory WHERE username = '$local_username' ORDER BY category ASC"; 
+										$result_set = mysqli_query($conn, $SQLSELECT); 
+										while ($rows = $result_set->fetch_assoc()) { 
+											$category = $rows['category']; 
+											echo "<option value='$category'>$category</option>";
+										}
+									?>
+								</select>
 							</div>
-							<div class="form-group col">
-									<label for="description" class="col-md-2 col-form-label" style="color: black;"><strong>Descripción</strong></label>
-								<div class="col-md-10">
-										<textarea name="description" rows="5" class="form-control" placeholder="Descripción del Contenido del Enlace"></textarea>
-								</div>
+
+							<!-- Descripción -->
+							<div class="form-group col-md-3">
+								<label for="description" class="col-form-label" style="color: black;"><strong>Descripción</strong></label>
+								<textarea name="description" rows="5" class="form-control" placeholder="Descripción del Contenido del Enlace"></textarea>
 							</div>
-							<br>
-								<center><input type="submit" class="btn btn-success btn-block" name="save_link" value="Guardar"></input></center>
-						</form>
+						</div>
+
+						<br>
+						<center><input type="submit" class="btn btn-success btn-block" name="save_link" value="Guardar"></input></center>
+					</form>
+
 					</div>
 				</div>
 				<br><br>
