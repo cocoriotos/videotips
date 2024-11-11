96e248572c5d53bf84c3ee9a224a2f92db672a4b
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-11 00:51:45 -0500

    center

--- edit.php
+++ edit.php
@@ -46,8 +46,8 @@ include "nobackpage.php";
 								<textarea name="description" rows="2" class="form-control" placeholder="Descripción del Contenido"><?php echo $link['description']?></textarea><br>
 							</div>						
 					</div></center>
-							<center><input type="submit" class="btn btn-success btn-short" name="update_link" value="Actualizar"></input></center>
-							<input type="submit" class="btn btn-success btn-short" name="logout" value="Borrar" formaction="delete.php"></input><br>				
+							<input type="submit" class="btn btn-success btn-block" name="update_link" value="Actualizar"></input>
+							<input type="submit" class="btn btn-success btn-block" name="logout" value="Borrar" formaction="delete.php"></input><br>				
 				</form>
 			</div>
 		</div>
