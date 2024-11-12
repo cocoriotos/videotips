ee97ed28719a1ec670067e24b97e84495abd003b
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-11 22:37:06 -0500

    5

--- edit.php
+++ edit.php
@@ -25,23 +25,23 @@ include "nobackpage.php";
 		  	<form  text-align="center"  action="updatelinks.php" method="POST"> 
 				<center><label for="title" class="col-form-label" style="color: black; font-size: 28px;"><strong> Editar Enlace </strong></label></center>
 					<center><div class="row">
-									<div class="form-group col-md-4">
+									<div class="form-group col-md-5">
 										<label for="id" class="col-form-label" style="color: black;"><strong>Id</strong></label><br>	
 										<input style="text-align: center;" type="text" name="id" class="form-control" placeholder="ID" autofocus value ="<?php echo $link['id'];?>" readonly></input><br>
 									</div>
-									<div class="form-group col-md-4">
+									<div class="form-group col-md-5">
 										<label for="videolink" class="col-form-label" style="color: black;"><strong>Enlace o URL</strong></label><br>	
 										<input type="text" name="videolink" class="form-control" placeholder="Enlace" autofocus value ="<?php echo $link['videolink'];?>"readonly></input><br>
 									</div>
-									<div class="form-group col-md-4">
+									<div class="form-group col-md-5">
 										<label for="maincategory" class="col-form-label" style="color: black;"><strong>Categoría</strong></label><br>
 										<select class="form-control" name="maincategory" required><?php $query_options = "SELECT distinct(maincategory) FROM videotips_maincategory where username = '$local_username' order by maincategory asc"; $result_options = mysqli_query($conn, $query_options); while ($option = mysqli_fetch_assoc($result_options)) { $selected = ($option['maincategory'] == $link['maincategory']) ? "selected" : ""; echo "<option value=\"{$option['maincategory']}\" $selected>{$option['maincategory']}</option>"; } ?></select><br><br>
 									</div>
-									<div class="form-group col-md-4">
+									<div class="form-group col-md-5">
 										<label for="category" class="col-form-label" style="color: black;"><strong>Subcategoría</strong></label><br>
 										<select class="form-control" name="category" required><?php $query_options = "SELECT distinct(category) FROM videotips_viodetipscategory where username = '$local_username' order by category asc"; $result_options = mysqli_query($conn, $query_options); while ($option = mysqli_fetch_assoc($result_options)) { $selected = ($option['category'] == $link['category']) ? "selected" : ""; echo "<option value=\"{$option['category']}\" $selected>{$option['category']}</option>"; } ?></select><br><br>
 									</div>
-									<div class="form-group col-md-4">
+									<div class="form-group col-md-5">
 										<label for="description" class="col-form-label" style="color: black;"><strong>Descripción</strong></label><br>	
 										<textarea name="description" rows="2" class="form-control" placeholder="Descripción del Contenido"><?php echo $link['description']?></textarea><br>
 									</div>						
