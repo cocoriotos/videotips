05116072132663269a0f1d15c6ad144f29f5ac4e
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-15 22:59:35 -0500

    ok

--- videolinkadminmodule.php
+++ videolinkadminmodule.php
@@ -8,7 +8,31 @@ include "db_connection1.php";
 $local_username=$_SESSION['email'];
 /*include "nobackpage.php";*/
 include "SessionTimeOut.php";
-?>
+if ($savedlink == 1) {
+	?>
+	<html>
+		<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+	
+		<!-- Scripts de alertify.js -->
+		<script src="https://cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/alertify.min.js"></script>
+		
+		<script>
+		// Usamos jQuery para asegurarnos de que el documento esté listo
+		$(document).ready(function() {
+			// Mostrar el mensaje de éxito en la parte superior central inmediatamente
+			alertify.notify('Enlace Adicionado Exitosamente', 'success', 5);
+		});
+		</script>
+		
+		<!-- Estilos de alertify.js -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/alertify.min.css"/>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/themes/default.min.css"/>
+	</html>
+	<?php 
+	}
+	$savedlink=0; 
+    ?>
+
 <head>	
 	<script src="head.js" defer></script>	
 	<script src="Linktoclipboard.js" defer></script>
@@ -21,6 +45,10 @@ include "SessionTimeOut.php";
 	<script src="plugins/alertifyjs/alertify.min.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/alertify.min.js"></script>
 	<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+
+
+	
 </head>
 
 <body id="bodyadminmodule">
