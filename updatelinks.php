3d5a19d0ba8154444f66eb0dd6775acb40d73f0a
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-18 00:03:32 -0500

    done

--- updatelinks.php
+++ updatelinks.php
@@ -16,10 +16,12 @@ $resultado=$conn ->query($query);
 
 if ($resultado){
   $_SESSION['updatedlink'] = 1;
-  header("refresh:0; url=edit.php");
+  header("refresh:0; url=videolinkadminmodule.php");
+  exit();
     }
   else{
       $_SESSION['updatedlink'] = 2;
-      header("refresh:0; url=edit.php");
-      }
+      header("refresh:0; url=videolinkadminmodule.php");
+     exit(); 
+     }
 ?>
