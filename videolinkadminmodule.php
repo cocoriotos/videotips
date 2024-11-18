48e638b23aa9255ad56c609e86f676add9d4076d
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-18 01:27:48 -0500

    delete 0

--- videolinkadminmodule.php
+++ videolinkadminmodule.php
@@ -196,7 +196,7 @@ $_SESSION['updatedlink'] = 0;
 
 
 
-if ($deletedlink  == 0) {
+if ($deletedlink  == 1) {
     echo '
     <script src="https://cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
@@ -212,7 +212,7 @@ if ($deletedlink  == 0) {
     </script>';
 }
 
-if ($deletedlink == 2){
+if ($deletedlink == 0){
 	echo '
     <script src="https://cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
