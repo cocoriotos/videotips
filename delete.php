d7a4963089e313e6bef8377e338b8c12d2317ed9
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-18 01:02:40 -0500

    delete

--- delete.php
+++ delete.php
@@ -7,7 +7,7 @@ $deletedlink = $_SESSION['deletedlink'];
 /*$query = "update videotips_videotips SET active = 'No' and username = '$local_username' where id='$id'";*/
 $query = "delete from videotips_videotips where id='$id'";
 $result = mysqli_query($conn,$query);
-if (!$result) {
+if ($result) {
     $_SESSION['deletedlink']=1;
     header("refresh:0; url=videolinkadminmodule.php");  
     exit();
