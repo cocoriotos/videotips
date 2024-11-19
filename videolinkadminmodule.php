e836fd8d126c25103d89b0c797926f3b59c7e300
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-19 08:33:06 -0500

    $elapsed_time

--- videolinkadminmodule.php
+++ videolinkadminmodule.php
@@ -34,6 +34,7 @@ bootstrapCDN https://getbootstrap.com and then download then CDN via jsDeliver a
 			<div class="row justify-content-start">
 				<div class="col-md-12"> 
 					<div class="card card-body">
+						<?php print_r($elapsed_time);?> 
 							<form action="savelinks.php" method="POST">
 							<center><label for="title" class="col-form-label" style="color: black; font-size: 28px;"><strong> Adicionar Enlace </strong></label></center>
 								<div class="row">
