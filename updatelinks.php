12db1bf0fa67fc17959395e2e37630664c95e361
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-19 16:52:31 -0500

    true

--- updatelinks.php
+++ updatelinks.php
@@ -9,11 +9,10 @@ $category=$_POST["category"];
 $description=$_POST["description"];
 $updatedlink = $_SESSION['updatedlink'];
 
-
 $query="UPDATE videotips_videotips SET id = '$id', videolink = '$videolink', maincategory = '$maincategory', category = '$category', description = '$description', active = 'Yes' where id = '$id'";
 $resultado=$conn ->query($query);
 
-if ($resultado){
+if ($resultado ===TRUE){
   $_SESSION['updatedlink'] = 1;
   header("refresh:0; url=videolinkadminmodule.php");
   exit();
