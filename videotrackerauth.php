c6c68bd2d189b1e5cd7c18867bbb9b4773423e5c
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-18 20:30:05 -0500

    0

--- videotrackerauth.php
+++ videotrackerauth.php
@@ -56,7 +56,7 @@
 	</body>
 
 	<?php 
-	if ($useraccesssucced == 2){
+	if ($useraccesssucced == 0){
 		echo '
 		<script src="https://cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
