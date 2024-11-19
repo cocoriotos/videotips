b4e2cda3adfc43e7974b390494f97c772d58dc02
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-19 09:32:48 -0500

    STO

--- SessionTimeOut.php
+++ SessionTimeOut.php
@@ -1,22 +1,19 @@
 <?php
 // Establecer el tiempo de inactividad en segundos (15 minutos)
-session_start();
 $timeout_duration = 60;
-$sessiontimeoutreached = $_SESSION['sessiontimeoutreached'];
 
 // Verificar si la sesión está activa
+if (isset($_SESSION['LAST_ACTIVITY'])) {
+    // Calcular el tiempo de inactividad
+    $elapsed_time = time() - $_SESSION['LAST_ACTIVITY'];
 
-if ($elapsed_time > $timeout_duration) {
-    // Si el tiempo de inactividad excede el límite, cerrar la sesión
-        $_SESSION['sessiontimeoutreached'] = 1;
-    // Verificamos si la URL de la página anterior está disponible
-       
-        include("closetaskscon.php");
-        header("Location: videotrackerauth.php");
-        exit();
+    if ($elapsed_time > $timeout_duration) {
+        // Si el tiempo de inactividad excede el límite, cerrar la sesión
+		include("closetaskscon.php");
+        header("refresh:0; url=videotrackerauth.php"); // Redirige a la página de autenticación
+    }
 }
 
 // Actualizar el tiempo de la última actividad
 $_SESSION['LAST_ACTIVITY'] = time();
 ?>
-</html>
