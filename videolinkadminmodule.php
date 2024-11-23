72e1d233a0fee5b3bee425da9aa9f875a3460d25
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-23 16:13:05 -0500

    1

--- videolinkadminmodule.php
+++ videolinkadminmodule.php
@@ -129,7 +129,7 @@ bootstrapCDN https://getbootstrap.com and then download then CDN via jsDeliver a
 								<td id="tdlink" align="left"><a href="<?php echo $links['videolink']; ?>" target="_blank"><?php echo $links['videolink']; ?></a></td>
 								<!--<td id="eye" align="center"><i class="fa fa-eye" aria-hidden="true"></i></td>-->
 								<?php $videoUrl = $links['videolink']; ?>
-								<td id="eye" align="center"><button class="fa fa-eye"  onclick="showIframe()"></button></td>
+								<td id="eye" align="center"><button class="fa fa-eye"  onclick="showIframe1()"></button></td>
 								
 								
 								<!--<?php /*$videoUrl = $links['videolink'];*/ ?>
