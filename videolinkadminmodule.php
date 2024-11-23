aa5e4672e4e6b0147f62d8d609264fb41a998988
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-23 00:49:26 -0500

    tdleft

--- videolinkadminmodule.php
+++ videolinkadminmodule.php
@@ -108,7 +108,7 @@ bootstrapCDN https://getbootstrap.com and then download then CDN via jsDeliver a
 							while($links = mysqli_fetch_array($result_links)) { ?>
 							<tr>
 								<td align="center"><?php echo"<a href='edit.php?id={$links['id']}'>{$links['id']}"?></td>
-								<td align="left"><a href="<?php echo $links['videolink']; ?>" target="_blank"><?php echo $links['videolink']; ?></a></td>
+								<td id="tdlink" align="left"><a href="<?php echo $links['videolink']; ?>" target="_blank"><?php echo $links['videolink']; ?></a></td>
 								<!--<?php /*$videoUrl = $links['videolink'];*/ ?>
 								<td><iframe width="100%" height="0" src="<?php /*include("embedeedlinks.php");*/ ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></td>-->
 								<td align="center"><?php echo $links['maincategory'] ?></td>
