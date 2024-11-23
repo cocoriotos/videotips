dc36e1270d681266e7158252d68ff5bd4541d012
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-23 16:30:56 -0500

    click

--- embedeedlinks.php
+++ embedeedlinks.php
@@ -5,6 +5,10 @@
 $videoUrl = $_SESSION['videoUrl'];
 $embedUrl = $_SESSION['embedUrl'];*/
 
-echo $embedUrl = str_replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/", $videoUrl);
+if (click===1){
 
+echo $embedUrl = str_replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/", $videoUrl);
+}else{
+    echo "No Disponible";
+}
 ?>
