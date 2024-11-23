e0d5a9911b56640c2f000fb4f6496c45d4a31926
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-23 16:26:11 -0500

    embedeed

--- embedeedlinks.php
+++ embedeedlinks.php
@@ -1,9 +1,9 @@
 
 
 <?php
-session_start();
+/*session_start();
 $videoUrl = $_SESSION['videoUrl'];
-$embedUrl = $_SESSION['embedUrl'];
+$embedUrl = $_SESSION['embedUrl'];*/
 
 echo $embedUrl = str_replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/", $videoUrl);
 
