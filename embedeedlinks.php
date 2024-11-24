02cb3d0cd24db74b87051666e4523dc96a493941
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-23 20:41:52 -0500

    embedded

--- embedeedlinks.php
+++ embedeedlinks.php
@@ -7,10 +7,10 @@ $embedUrl = $_SESSION['embedUrl'];
 $click = $_SESSION['click'];
 
 $click=1;*/
-if ($click===1){
+/*if ($click===1){*/
 
 echo $embedUrl = str_replace("https://www.youtube.com/watch?v=", "https://www.youtube.com/embed/", $videoUrl);
-$click = 0;
+/*$click = 0;
 exit();
 }
 if ($click ===0)
@@ -18,5 +18,5 @@ if ($click ===0)
     echo "No Disponible";
     $click = 0;
     exit();
-}
+}*/
 ?>
