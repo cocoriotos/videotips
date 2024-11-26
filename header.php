9e49d2df54f498b9f771e41afe27e3aeea719760
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-26 15:22:58 -0500

    usd

--- header.php
+++ header.php
@@ -12,7 +12,7 @@ include "SessionTimeOut.php";*/
    <center><nav class="navbar navbar-dark bg-dark d-flex justify-content-center align-items-center">
 	        <div class="text-center">
                 <a id="headerfonts" href="#" class="btn" style="background-color: #28A745; color: white; font-weight: bold;" onclick="copiarNumero();"><i class="fas fa-mobile-alt"></i> Número Nequi: 3054293185</a>
-                <a id="headerfonts" href="suscriptionpayment.php" class="btn" style="background-color: #28A745; color: white; font-weight: bold;"><i class="fas fa-dollar"></i> Tarifas y pago por Nequi</a>
+                <a id="headerfonts" href="suscriptionpayment.php" class="btn" style="background-color: #28A745; color: white; font-weight: bold;"><i class="fas fa-usd"></i> Tarifas y pago por Nequi</a>
                 <a id="headerfonts" href="videolinkadminmodule.php" class="btn" style="background-color: #FFF9CC; color: black; font-weight: bold;"><i class="fas fa-broom"></i> Limpiar Formulario</a>
                 <a id="headerfonts" href="addcategory.php" class="btn" style="background-color: #D6EEFF; color: black; font-weight: bold;"><i class="fas fa-layer-group"></i> Categorías</a>
                 <a id="headerfonts" href="videotrackerauth.php" class="btn btn-danger" style="background-color: #FFD6D6; color: black; font-weight: bold;"><i class="fas fa-sign-out-alt"></i> Salir</a>
