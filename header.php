623214f3c438ff22905af07aa8f2909a86104bb8
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-26 15:34:15 -0500

    right left margin

--- header.php
+++ header.php
@@ -25,9 +25,9 @@ include "SessionTimeOut.php";*/
         <a id="headerfonts" href="suscriptionpayment.php" class="btn" style="background-color: #28A745; color: white; font-weight: bold;"><i class="fa fa-usd"></i> Tarifas y pago por Nequi</a>
     </div>
     <div class="d-flex">
-        <a id="headerfonts" href="videolinkadminmodule.php" class="btn" style="background-color: #FFF9CC; color: black; font-weight: bold; margin-right: 10px;"><i class="fas fa-broom"></i> Limpiar Formulario</a>
-        <a id="headerfonts" href="addcategory.php" class="btn" style="background-color: #D6EEFF; color: black; font-weight: bold; margin-right: 10px;"><i class="fas fa-layer-group"></i> Categorías</a>
-        <a id="headerfonts" href="videotrackerauth.php" class="btn btn-danger" style="background-color: #FFD6D6; color: black; font-weight: bold;"><i class="fas fa-sign-out-alt"></i> Salir</a>
+        <a id="headerfonts" href="videolinkadminmodule.php" class="btn" style="background-color: #FFF9CC; color: black; font-weight: bold; margin-left: 10px;"><i class="fas fa-broom"></i> Limpiar Formulario</a>
+        <a id="headerfonts" href="addcategory.php" class="btn" style="background-color: #D6EEFF; color: black; font-weight: bold; margin-left: 10px;"><i class="fas fa-layer-group"></i> Categorías</a>
+        <a id="headerfonts" href="videotrackerauth.php" class="btn btn-danger" style="background-color: #FFD6D6; color: black; font-weight: bold; margin-left: 10px;"><i class="fas fa-sign-out-alt"></i> Salir</a>
     </div>
 </nav>
 
