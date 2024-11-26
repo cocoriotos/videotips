2e4975af62464aa018d2eaab7a80da80da84e743
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-26 15:18:20 -0500

    rollback

--- header.php
+++ header.php
@@ -9,23 +9,15 @@ include "SessionTimeOut.php";*/
 	    <center><a id="welcome" href="videolinkadminmodule.php" class="navbar-brand">Bienvenido a tu Biblioteca de Contenidos Útiles</a></center>
   </nav>
   <script src="copynumber.js"></script>
-  <left><nav class="navbar navbar-dark bg-dark d-flex justify-content-center align-items-center">
+   <center><nav class="navbar navbar-dark bg-dark d-flex justify-content-center align-items-center">
 	        <div class="text-center">
                 <a id="headerfonts" href="#" class="btn" style="background-color: #28A745; color: white; font-weight: bold;" onclick="copiarNumero();"><i class="fas fa-mobile-alt"></i> Número Nequi: 3054293185</a>
-                <a id="headerfonts" href="suscriptionpayment.php" class="btn" style="background-color: #28A745; color: white; font-weight: bold;"><i class="fas fa-file-text"></i> Tarifas y pago por Nequi</a>
-                <a id="headerfonts" href="videolinkadminmodule.php" class="btn" style="background-color: #FFF9CC; color: black; font-weight: bold;"><i class="fas fa-broom"></i> Limpiar Formulario</a>
-            </div>	
-  </nav></left>
-
-  <right><nav class="navbar navbar-dark bg-dark d-flex justify-content-center align-items-center">
-	        <div class="text-center">
-                <a id="headerfonts" href="#" class="btn" style="background-color: #28A745; color: white; font-weight: bold;" onclick="copiarNumero();"><i class="fas fa-mobile-alt"></i> Número Nequi: 3054293185</a>
-                <a id="headerfonts" href="suscriptionpayment.php" class="btn" style="background-color: #28A745; color: white; font-weight: bold;"><i class="fas fa-file-text"></i> Tarifas y pago por Nequi</a>
+                <a id="headerfonts" href="suscriptionpayment.php" class="btn btn-dollar" style="background-color: #28A745; color: white; font-weight: bold;"><i class="fas fa-file-text"></i> Tarifas y pago por Nequi</a>
                 <a id="headerfonts" href="videolinkadminmodule.php" class="btn" style="background-color: #FFF9CC; color: black; font-weight: bold;"><i class="fas fa-broom"></i> Limpiar Formulario</a>
                 <a id="headerfonts" href="addcategory.php" class="btn" style="background-color: #D6EEFF; color: black; font-weight: bold;"><i class="fas fa-layer-group"></i> Categorías</a>
                 <a id="headerfonts" href="videotrackerauth.php" class="btn btn-danger" style="background-color: #FFD6D6; color: black; font-weight: bold;"><i class="fas fa-sign-out-alt"></i> Salir</a>
             </div>	
-  </nav></right>
+  </nav></center>
 
   <!-- BOOTSTRAP -->	
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
