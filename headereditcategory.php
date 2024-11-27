0f663937cd7f4b1275f043c70c79860d458940d9
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-26 20:32:11 -0500

    between

--- headereditcategory.php
+++ headereditcategory.php
@@ -9,7 +9,7 @@ include "SessionTimeOut.php";*/
 	    <center><a id="welcome" href="videolinkadminmodule.php" class="navbar-brand">Bienvenido a tu Biblioteca de Contenidos Útiles</a></center>
 </nav>
 
-<<nav class="navbar navbar-dark bg-dark d-flex justify-content-center align-items-center">
+<<nav class="navbar navbar-dark bg-dark d-flex justify-content-between align-items-center">
 <div class="d-flex"></div>  
   <div class="d-flex">
       <a id="headerfonts" href="videolinkadminmodule.php" class="btn" style="background-color: #FFF9CC; color: black; font-weight: bold; margin-right: 10px;"><i class="fas fa-reply"></i> Adicionar Enlace</a>
