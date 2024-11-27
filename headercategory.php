53694ce521363fef019a2712cb3bbdd4be165830
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-26 20:29:40 -0500

    hc

--- headercategory.php
+++ headercategory.php
@@ -9,17 +9,17 @@ include "SessionTimeOut.php";*/
   <center><a id="welcome" href="videolinkadminmodule.php" class="navbar-brand">Bienvenido a tu Biblioteca de Contenidos Útiles</a></center>
   </nav>
 
-    <nav class="navbar navbar-dark bg-dark d-flex justify-content-center align-items-center">
+    <nav class="navbar navbar-dark bg-dark d-flex justify-content-between align-items-center">
     <div class="d-flex"></div>          
-    <div class="d-flex">
+            <div class="d-flex">
                 <a id="headerfonts" href="addcategory.php" class="btn" style="background-color: #FFF9CC; color: black; font-weight: bold; margin-left: 10px;"><i class="fas fa-broom"></i> Limpiar Formulario</a>
                 <a id="headerfonts" href="videolinkadminmodule.php" class="btn" style="background-color: #FFD6D6; color: black; font-weight: bold; margin-left: 10px;"><i class="fas fa-times"></i> Cancelar</a>
                 <a id="headerfonts" href="videotrackerauth.php" class="btn btn-danger" style="background-color: #D6EEFF; color: black; font-weight: bold; margin-left: 10px; margin-right: 20px;"><i class="fas fa-sign-out-alt"></i> Salir</a>
-              </div>   
+            </div>   
     </nav>
   
   <!-- BOOTSTRAP -->	
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
     <script src="https://kit.fontawesome.com/60f0db780e.js" crossorigin="anonymous"></script>
 	</header>
-    </html>
+  </html>
--- headeredit.php
+++ headeredit.php
@@ -21,5 +21,5 @@ include "SessionTimeOut.php";*/
   <!-- BOOTSTRAP -->	
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
     <script src="https://kit.fontawesome.com/60f0db780e.js" crossorigin="anonymous"></script>
-	</header>
-    </html>
+</header>
+</html>
