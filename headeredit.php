3fb814574d72d81f1491e14b4bf110b255adab01
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-26 19:38:00 -0500

    ec

--- headeredit.php
+++ headeredit.php
@@ -16,7 +16,7 @@ include "SessionTimeOut.php";*/
                     <a id="headerfonts" href="videolinkadminmodule.php" class="btn" style="background-color: #FFD6D6; color: black; font-weight: bold; margin-right: 10px;"><i class="fas fa-times"></i> Cancelar</a>
                     <a id="headerfonts" href="videotrackerauth.php" class="btn btn-danger" style="background-color: #D6EEFF; color: black; font-weight: bold; margin-left: 10px; margin-right: 20px;"><i class="fas fa-sign-out-alt"></i> Salir</a>
               </div>
-    </nav><
+    </nav>
     
   <!-- BOOTSTRAP -->	
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
