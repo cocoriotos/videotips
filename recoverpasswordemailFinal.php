096beabf4eb10023fb7677df9b55401495b7430c
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-30 02:53:08 -0500

    contraseña

--- recoverpasswordemailFinal.php
+++ recoverpasswordemailFinal.php
@@ -5,8 +5,11 @@ include ("db_connection1.php");
 
 $email = $_POST['email'];
 
-$query3="SELECT * from videotips_app_access_list where email = '$email'";
+$query3="SELECT password from videotips_app_access_list where email = '$email'";
 $result3=$conn->query($query3);
+
+$row = $result3->fetch_assoc();
+$password = $row['password'];
 
 if($result3->num_rows > 0){ 
 /*Destination email information*/
@@ -28,17 +31,13 @@ $mail = mail($to, $subject, $message,$header);
 
 
 	$to = "$email";
-	$subject = "Requerimiento de acceso";
-	$message = "Buen día $lastname, $name :  \n\n";
-	$message.= "Su requerimiento ha sido enviado a los administradores de la herramienta  para processar su solicitud. Acá la información de su solicitud \n\n";//. to concatenate lines in the same variable
-	$message.= str_pad("Nombre", 40) . ": $name \n";
-	$message.= str_pad("Apellido", 40) . ": $lastname \n";
+	$subject = "Solicitud de Recuperación de Password";
+	$message = "Buen día $email :  \n\n";
+	$message.= "Su requerimiento de recuperación de Contraseña ha sido procesada. Acá la información de su solicitud \n\n";//. to concatenate lines in the same variable
 	$message.= str_pad("Email", 40) . ": $email \n";
-	$message.= str_pad("País", 40) . ": $country \n";
-	$message.= str_pad("Ciudad", 40) . ": $city \n";
-	$message.= str_pad("Nombre de usuario  asignado", 40) . ": $email \n\n";
+	$message.= str_pad("Contraseña", 40) . ": $password \n\n";
 	$message.= "Por favor no responder éste correo \n\n";
-	$message.= "Gracias por su registro, ya puede ingresar a la app desde este enlace https://solicionespro.com/videotips/videotrackerauth.php con su usuario $email  y la contraseña que escogió. \n\n";
+	$message.= "Gracias por su solicitud, ya puede ingresar a la app desde este enlace https://solicionespro.com/videotips/videotrackerauth.php con su usuario $email  y la contraseña correspondiente. \n\n";
 	$header = "From: adm@solicionespro.com" . "\r\n";
 	$header.= "Bcc: cocoriotos@hotmail.com\r\n";
 	$header.= "X-Mailer: PHP/". phpversion();
