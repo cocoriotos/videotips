9de5615e9d585c22adac1ed4fcea91b1584afc96
Julian GOnzalez <cocoriotos@hotmail.com>
2024-11-30 01:37:20 -0500

    br

--- recoverpassword.php
+++ recoverpassword.php
@@ -22,7 +22,7 @@
 			<HR/>	
 	</header>	
 	  <body id="bodyadminmodule">
-	  <br><br><br><br><br><br><br>
+	  <br><br><br><br><br><br><br><br><br><br>
 		<form id="login" action=".php" method="POST" autocomplete="off"> 
 			    <center>
 					<font color=lightblue id="form_title"><strong>Recuperación de Contraseña</strong></font><br><br>
@@ -39,7 +39,7 @@
 					<br>
 				</center>
 		</form>	
-		<br>
+		<br><br><br><br><br>
 		<footer id="contact">	
 					<hgroup>
 					<HR id="HR"/>
