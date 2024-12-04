69dfcf567039cbac55df54027dbc964a463acf74
Julian GOnzalez <cocoriotos@hotmail.com>
2024-12-04 00:45:10 -0500

    br

--- videotrackerauth.php
+++ videotrackerauth.php
@@ -22,7 +22,7 @@
 			
 	</header>	
 	  <body id="bodyadminmodule">
-	  <br>
+	  <br><br><br><br>
 		<form id="login" action="access_success_Tasks_final.php" method="POST" autocomplete="off"> 
 			    <center>
 					<font color=lightblue id="form_title"><strong>Formulario de Autenticación</strong></font><br><br>
