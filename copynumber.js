34906f86c77e4083dd7c4faf9c24d56b41aeeeed
Julian GOnzalez <cocoriotos@hotmail.com>
2024-12-04 22:01:26 -0500

    paypal

--- copynumber.js
+++ copynumber.js
@@ -1,6 +1,15 @@
 function copiarNumero() {
-	var telefono = "3054293185"; 
-	navigator.clipboard.writeText(telefono).then(function() {
-	}, function(err) {
-	});
+  var telefono = "3054293185";
+  navigator.clipboard.writeText(telefono).then(
+    function () {},
+    function (err) {}
+  );
+}
+
+function copiarPaypal() {
+  var paypal = "YSXRZMT2AAG4G";
+  navigator.clipboard.writeText(paypal).then(
+    function () {},
+    function (err) {}
+  );
 }
