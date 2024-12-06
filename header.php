1fff9aa43f78edc1d2e8346289bf9a34c994e0cb
Julian GOnzalez <cocoriotos@hotmail.com>
2024-12-06 01:54:06 -0500

    br

--- header.php
+++ header.php
@@ -21,8 +21,8 @@ include "SessionTimeOut.php";*/
         <a id="headerfonts" href="videolinkadminmodule.php" class="btn" style="background-color: #FFF9CC; color: black; font-weight: bold; margin-left: 10px;"><i class="fas fa-broom"></i> Limpiar Formulario</a>
         <a id="headerfonts" href="addcategory.php" class="btn" style="background-color: #D6EEFF; color: black; font-weight: bold; margin-left: 10px;"><i class="fas fa-layer-group"></i> Categorías</a>
         <a id="headerfonts" href="videotrackerauth.php" class="btn btn-danger" style="background-color: #FFD6D6; color: black; font-weight: bold; margin-left: 10px; margin-right: 20px;"><i class="fas fa-sign-out-alt"></i> Salir</a>
-    </div>
-    <br>
+        <br>
+      </div>
 </nav>
 
 
