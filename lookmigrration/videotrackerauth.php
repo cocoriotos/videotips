d3208236c3d9c36a44cd5afc564a499742a40c62
Julian GOnzalez <cocoriotos@hotmail.com>
2025-02-06 16:25:24 -0500

    p

--- lookmigrration/videotrackerauth.php
+++ lookmigrration/videotrackerauth.php
@@ -36,7 +36,7 @@
                     <input type="password" name="password" placeholder="Contraseña" required>
                 </div>
                 <button type="submit" class="btn-login">Ingresar</button>
-                <a href="recoverpassword.php" class="forgot-password">¿Olvidaste tu contraseña?</a>
+                <p>< href="recoverpassword.php" class="forgot-password">¿Olvidaste tu contraseña?</p>
             </form>
             <br>
             <form id="request-access" action="requestaccessfinal.php" method="POST" autocomplete="off">
