e26e65eaa0117187cb3848e89f7313bc09a98234
Julian GOnzalez <cocoriotos@hotmail.com>
2025-02-07 23:49:41 -0500

    suscriptions

--- lookmigrration/suscriptionpayment.php
+++ lookmigrration/suscriptionpayment.php
@@ -120,7 +120,7 @@ if ($suscriptiondue == 1) {
     document.addEventListener('DOMContentLoaded', function() {
       Swal.fire({
         title: 'Mensaje',
-        text: 'Suscripción inactiva. Se sugiere renovarla',
+        text: 'Has alcanzado el límite de 15 días de uso gratuito y/o tu Suscripción está vencida. Te invitamos a renovarla por medio nuestros canales de Nequi  para Colombia o Paypayl para Internacional',
         icon: 'error',
         confirmButtonText: 'Aceptar',
         customClass: {
