d9878381280e0b4f532eecec5a1d3c2489971c6e
Julian GOnzalez <cocoriotos@hotmail.com>
2025-02-16 02:02:00 -0500

    icon

--- lookmigrration/index.php
+++ lookmigrration/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SmartShelf - Tu Biblioteca Digital Inteligente</title>
     <link rel="stylesheet" href="style_sheet_landing_page.css">
+    <link rel="icon" href="SSCircleBackgroundWhite.ico" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
