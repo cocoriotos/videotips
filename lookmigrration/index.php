67d600d4781b630690b3597fc26ea59f92a4a1d3
Julian GOnzalez <cocoriotos@hotmail.com>
2025-02-16 13:13:04 -0500

    fix

--- lookmigrration/index.php
+++ lookmigrration/index.php
@@ -50,8 +50,8 @@
                     <h3>Uso de la Aplicación</h3>
                     <p>Videos cortos que te ayudan a saber cómo usar la aplicación paso a paso.</p>
                     <div class="button-container">
-                        <a href="https://www.youtube.com/playlist?list=PLRQ5KF9igtB2GRlHLSP6Uwx1lzy387Wz5" class="btn" target="_blank">Videos Tutoriales</a>
-                        <a href="UCLToolManualDelUsuario2025.pdf" class="btn" target="_blank">Manual del Usuario</a>
+                        <a href="https://www.youtube.com/playlist?list=PLRQ5KF9igtB2GRlHLSP6Uwx1lzy387Wz5" class="btn1" target="_blank">Videos Tutoriales</a>
+                        <a href="UCLToolManualDelUsuario2025.pdf" class="btn1" target="_blank">Manual del Usuario</a>
                     </div>
                 </div>
             </div>
