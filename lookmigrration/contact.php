bebdeaea0635d343e37ec79b94c8d2f717961d28
Julian GOnzalez <cocoriotos@hotmail.com>
2025-02-16 21:51:37 -0500

    message format

--- lookmigrration/contact.php
+++ lookmigrration/contact.php
@@ -1,4 +1,5 @@
 <?php
+echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
@@ -12,39 +13,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $headers = "From: $email";
 
     if (mail($to, $subject, $body, $headers)) {
-        echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'>;
-                    Swal.fire({
-                        title: 'Mensaje',
-                        text: 'Gracias por contactarnos, Estamso revisando tu mensaje y te responderemos pronto.',
-                        icon: 'success',
-                        confirmButtonText: 'Aceptar',
-                        customClass: {
-                            popup: 'custom-swal-popup',
-                            title: 'custom-swal-title',
-                            content: 'custom-swal-content',
-                            confirmButton: 'custom-swal-confirm-button'
-                        }
-                    }).then(() => {
-						window.location.href = 'index.php';
-                    });
-        </script>";
+        // Alerta de éxito y redirección
+        echo "<script>
+                Swal.fire({
+                    title: 'Mensaje',
+                    text: 'Gracias por contactarnos, estamos revisando tu mensaje y te responderemos pronto.',
+                    icon: 'success',
+                    confirmButtonText: 'Aceptar',
+                    customClass: {
+                        popup: 'custom-swal-popup',
+                        title: 'custom-swal-title',
+                        content: 'custom-swal-content',
+                        confirmButton: 'custom-swal-confirm-button'
+                    }
+                }).then(() => {
+                    window.location.href = 'index.php';
+                });
+              </script>";
     } else {
-        echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'>;
-                    Swal.fire({
-                        title: 'Mensaje',
-                        text: 'Hubo un error al enviar tu mensaje. Inténtalo de nuevo.',
-                        icon: 'error',
-                        confirmButtonText: 'Aceptar',
-                        customClass: {
-                            popup: 'custom-swal-popup',
-                            title: 'custom-swal-title',
-                            content: 'custom-swal-content',
-                            confirmButton: 'custom-swal-confirm-button'
-                        }
-                    }).then(() => {
-						window.location.href = 'index.php';
-                    });
-        </script>";
+        // Alerta de error y redirección
+        echo "<script>
+                Swal.fire({
+                    title: 'Mensaje',
+                    text: 'Hubo un error al enviar tu mensaje. Inténtalo de nuevo.',
+                    icon: 'error',
+                    confirmButtonText: 'Aceptar',
+                    customClass: {
+                        popup: 'custom-swal-popup',
+                        title: 'custom-swal-title',
+                        content: 'custom-swal-content',
+                        confirmButton: 'custom-swal-confirm-button'
+                    }
+                }).then(() => {
+                    window.location.href = 'index.php';
+                });
+              </script>";
     }
 }
 ?>
