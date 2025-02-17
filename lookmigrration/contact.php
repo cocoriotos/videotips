3ca9448b2f7659994f5fee63d65c5f444c188f30
Julian GOnzalez <cocoriotos@hotmail.com>
2025-02-17 15:31:15 -0500

    contact

--- lookmigrration/contact.php
+++ lookmigrration/contact.php
@@ -1,5 +1,4 @@
 <?php
-echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
@@ -14,36 +13,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (mail($to, $subject, $body, $headers)) {
         // Alerta de éxito y redirección
+        echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
         echo "<script>
                 Swal.fire({
                     title: 'Mensaje',
                     text: 'Gracias por contactarnos, estamos revisando tu mensaje y te responderemos pronto.',
                     icon: 'success',
-                    confirmButtonText: 'Aceptar',
-                    customClass: {
-                        popup: 'custom-swal-popup',
-                        title: 'custom-swal-title',
-                        content: 'custom-swal-content',
-                        confirmButton: 'custom-swal-confirm-button'
-                    }
+                    confirmButtonText: 'Aceptar'
                 }).then(() => {
                     window.location.href = 'index.php';
                 });
               </script>";
     } else {
         // Alerta de error y redirección
+        echo "<script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>";
         echo "<script>
                 Swal.fire({
                     title: 'Mensaje',
                     text: 'Hubo un error al enviar tu mensaje. Inténtalo de nuevo.',
                     icon: 'error',
-                    confirmButtonText: 'Aceptar',
-                    customClass: {
-                        popup: 'custom-swal-popup',
-                        title: 'custom-swal-title',
-                        content: 'custom-swal-content',
-                        confirmButton: 'custom-swal-confirm-button'
-                    }
+                    confirmButtonText: 'Aceptar'
                 }).then(() => {
                     window.location.href = 'index.php';
                 });
