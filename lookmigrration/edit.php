bcc9c400092d63ecd439673ed2a2fce9b3b9756d
Julian GOnzalez <cocoriotos@hotmail.com>
2025-02-19 23:15:54 -0500

    edit

--- lookmigrration/edit.php
+++ lookmigrration/edit.php
@@ -103,9 +103,9 @@ include "SessionTimeOut.php";*/
             <div class="col-md-12">
                 <br>
                 <div class="card card-body">
-                    <table class="table table-bordered" id="tableswhite">
-                        <center><label for="description" class="col-form-label" style="color: black; font-size: 28px;"><strong> Información de Enlace a Modificar </strong></label></center>
-                        <thead>
+                    <table id="autosearch" class="display" font color="back">
+                        <center><label for="maincategory" class="col-form-label" style="color: black; font-size: 28px;"><strong> Información de Enlace a Modificar </strong></label></center>
+                        <thead id="tableswhite">
                             <tr>
                                 <th>ID</th>
                                 <th>Enlace o URL</th>
