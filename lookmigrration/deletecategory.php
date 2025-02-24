0e152f4a6384c83166b54ac89e624063d2bd492f
Julian GOnzalez <cocoriotos@hotmail.com>
2025-02-24 17:52:27 -0500

    deletecategory

--- lookmigrration/deletecategory.php
+++ lookmigrration/deletecategory.php
@@ -1,15 +1,13 @@
 <?php 
 include "db_connection1.php";
 session_start();
-$id = $_GET['id'];
-$maincategory = $_GET['maincategory'];
-$category = $_GET['category'];
+$id = $_POST['id'];
 $local_username=$_SESSION['email'];
 $deletedcategory = $_SESSION['deletedcategory'];
 
 
 
-$query = "delete from videotips_viodetipscategory1 where id='$id'";
+$query = "delete from videotips_viodetipscategory where id='$id'";
 $result = mysqli_query($conn,$query);
 
 
