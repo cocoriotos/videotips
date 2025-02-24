729a5292f04a67ea418fbacae949dff75bdf5ad2
Julian GOnzalez <cocoriotos@hotmail.com>
2025-02-23 22:43:42 -0500

    delete

--- lookmigrration/delete.php
+++ lookmigrration/delete.php
@@ -1,11 +1,11 @@
 <?php 
 session_start();
 include "db_connection1.php";
-$id=$_POST['id'];
-$videolink=$_POST['videolink'];
+$id = $_POST['id'];
+$videolink = $_POST['videolink'];
 $local_username=$_SESSION['username'];
 $deletedlink = $_SESSION['deletedlink'];
-$query = "delete from videotips_videotips where videolink='$videolink' and id='$id'";
+$query = "delete from videotips_videotips where videolink = '$videolink'";
 $result = mysqli_query($conn,$query);
 if ($result) {
   $_SESSION['deletedlink'] = 1; 
