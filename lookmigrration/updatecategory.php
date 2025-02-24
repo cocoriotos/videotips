da0b75bf3bc91438d9ede074d6cb799153597a68
Julian GOnzalez <cocoriotos@hotmail.com>
2025-02-24 01:32:39 -0500

    categoyupdate

--- lookmigrration/updatecategory.php
+++ lookmigrration/updatecategory.php
@@ -2,14 +2,16 @@
 session_start();
 include "db_connection1.php"; 
 
-$id = $_POST['id'];
+$maincategory = $_POST['maincategory'];
+$category = $_GET['category'];
+$local_username=$_SESSION['email'];
 $maincategory=$_POST["maincategory"];
 $category=$_POST["category"];
 $updatedcategory = $_SESSION['updatedcategory'];
 
 
 
-$query="UPDATE videotips_viodetipscategory SET id = '$id', maincategory = '$maincategory', category = '$category' where id = '$id'";
+$query="UPDATE videotips_viodetipscategory SET maincategory = '$maincategory', category = '$category' where maincategory = $maincategory and category = $category and username = '$local_username'";
 $resultado=$conn ->query($query);
 
 if ($resultado){
