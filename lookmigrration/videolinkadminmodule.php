aaaede8c0e6fece01484ef4c91ffd7a2f988fdcb
Julian GOnzalez <cocoriotos@hotmail.com>
2025-02-28 13:58:19 -0500

    les decription

--- lookmigrration/videolinkadminmodule.php
+++ lookmigrration/videolinkadminmodule.php
@@ -115,7 +115,6 @@
                                     <p><strong>Categoría:</strong> <?php echo $links['maincategory']; ?></p>
                                     <p><strong>Subcategoría:</strong> <?php echo $links['category']; ?></p>
                                     <p><strong>Contenido:</strong> <?php echo $links['proforpers']; ?></p>
-                                    <p><strong>Descripción:</strong> <?php echo $links['content']; ?></p>
                                     <p><strong>Creación:</strong> <?php echo $links['creationdate']; ?></p>
                                     <a href="<?php echo $links['videolink']; ?>" target="_blank" class="btn btn-primary">Ir al Contenido</a>
                                 
