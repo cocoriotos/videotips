5de5af60be7f37d9a4f631f6b1cb4b97c0033483
Julian GOnzalez <cocoriotos@hotmail.com>
2025-02-28 10:53:05 -0500

    new grid veiw

--- lookmigrration/videolinkadminmodule.php
+++ lookmigrration/videolinkadminmodule.php
@@ -90,40 +90,34 @@
                 <?php include("search.php") ?>
                 <div class="card card-body" div="card-body">
                     <center><label for="description" class="col-form-label" style="color: black; font-size: 28px;"><strong> Tus Enlaces Útiles </strong></label></center>
-                    <table id="autosearch" class="display">
-                        <thead id="tableswhite">
-                            <tr>
-                                <!--<th>ID</th>-->
-                                <th>Enlace</th>
-                                <th>Categoría</th>
-                                <th>Subcategoría</th>
-                                <th>Contenido</th>
-                                <th>Descripción</th>
-                                <th>Creación</th>
-                                <th>Copiar Enlace</th>
-                                <th>Acciones</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php 
-                            $query1 = "select * from videotips_videotips where active = 'Yes' and username ='$local_username' order by maincategory, category asc";
-                            $result_links = mysqli_query($conn,$query1);                            
-                            while($links = mysqli_fetch_array($result_links)) { ?>
-                            <tr>
-                                <!--<td align="center"><?php /*echo"<a href='edit.php?id={$links['id']}'>{$links['id']}"*/?></td>-->
-                                <td id="tdlink" align="left"><a href="<?php echo $links['videolink']; ?>" target="_blank"><?php echo $links['videolink']; ?></a></td>
-                                <td id="tdmaincategory" align="center"><?php echo $links['maincategory'] ?></td>
-                                <td id="tdcategory" align="center"><?php echo $links['category'] ?></td>
-                                <td id="tdproforpers" align="center"><?php echo $links['proforpers'] ?></td>
-                                <td id="tddescription" align="left"><?php echo $links['content'] ?></td>
-                                <td align="center"><?php echo $links['creationdate'] ?></td>
-                                <td align="center"><button class="fas fa-copy color-dark-icon" title = "Copiar" onclick="copyToClipboard('<?php echo $links['videolink']; ?>')"></button></td>
-                                <td><a href="edit.php?id=<?php echo $links['id']?>"class="btn btn-secondary" title="Modificar"><i class="fas fa-marker"></i></a>
-						                        <!--<a href="videoadminmodule.php?id=<?php echo $links['id']?>"class="btn btn-danger" title="Borrar"><i class="far fa-trash-alt"></i></a></td>-->
-                            </tr>
-                            <?php }?>
-                        </tbody>
-                    </table>
+                    <div class="grid-container">
+                        <?php 
+                        $query1 = "select * from videotips_videotips where active = 'Yes' and username ='$local_username' order by maincategory, category asc";
+                        $result_links = mysqli_query($conn,$query1);                            
+                        while($links = mysqli_fetch_array($result_links)) { ?>
+                        <div class="grid-item">
+                            <div class="grid-item-content">
+                                <div class="grid-item-header">
+                                    <span class="grid-item-title"><?php echo $links['content']; ?></span>
+                                    <div class="grid-item-actions">
+                                        <button class="grid-item-action-btn" onclick="toggleActions(<?php echo $links['id']; ?>)">...</button>
+                                        <div class="grid-item-action-menu" id="action-menu-<?php echo $links['id']; ?>">
+                                            <button onclick="copyToClipboard('<?php echo $links['videolink']; ?>')">Copiar Enlace</button>
+                                            <a href="edit.php?id=<?php echo $links['id']?>" class="btn btn-secondary">Modificar</a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="grid-item-body">
+                                    <p><strong>Categoría:</strong> <?php echo $links['maincategory']; ?></p>
+                                    <p><strong>Subcategoría:</strong> <?php echo $links['category']; ?></p>
+                                    <p><strong>Contenido:</strong> <?php echo $links['proforpers']; ?></p>
+                                    <p><strong>Descripción:</strong> <?php echo $links['content']; ?></p>
+                                    <p><strong>Creación:</strong> <?php echo $links['creationdate']; ?></p>
+                                </div>
+                            </div>
+                        </div>
+                        <?php }?>
+                    </div>
                 </div>
             </div>
         </div>
@@ -177,7 +171,17 @@ window.onload = function() {
     var maincategory = document.getElementById("maincategory").value;
     getSubcategories(maincategory);
 };
+
+function toggleActions(id) {
+    var actionMenu = document.getElementById("action-menu-" + id);
+    if (actionMenu.style.display === "block") {
+        actionMenu.style.display = "none";
+    } else {
+        actionMenu.style.display = "block";
+    }
+}
 </script>
+
 
 <?php
 
