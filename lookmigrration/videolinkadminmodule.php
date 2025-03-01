0efb10100f2cd71a675ec2cd1b2618f46486c6dc
Julian GOnzalez <cocoriotos@hotmail.com>
2025-02-28 23:57:31 -0500

    hr

--- lookmigrration/videolinkadminmodule.php
+++ lookmigrration/videolinkadminmodule.php
@@ -103,7 +103,7 @@
                                 <button class="grid-item-action-btn" onclick="toggleActions(<?php echo $links['id']; ?>)">...</button>
                                 <div class="grid-item-header">
                                     <span class="grid-item-title"><?php echo $links['content']; ?></span>
-                                    <hr
+                                    <hr>
                                     <div class="grid-item-actions">
                                         <div class="grid-item-action-menu" id="action-menu-<?php echo $links['id']; ?>">
                                             <button onclick="copyToClipboard('<?php echo $links['videolink']; ?>')" class="btn btn-secondary">Copiar Enlace</button>
