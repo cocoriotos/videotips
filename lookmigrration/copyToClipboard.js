ba73da81f58366f63e2652f5b91231f17df4c87c
Julian GOnzalez <cocoriotos@hotmail.com>
2025-03-01 12:42:53 -0500

    roll back

--- lookmigrration/copyToClipboard.js
+++ lookmigrration/copyToClipboard.js
@@ -1,17 +1,8 @@
-function copyToClipboard(text, menuId) {
-  // Copiar el texto al portapapeles
-  navigator.clipboard
-    .writeText(text)
-    .then(function () {
-      console.log("Enlace copiado al portapapeles: " + text);
-    })
-    .catch(function (error) {
-      console.error("Error al copiar el enlace: ", error);
-    });
-
-  // Ocultar el menú de acciones
-  var actionMenu = document.getElementById(menuId);
-  if (actionMenu) {
-    actionMenu.classList.remove("show"); // Ocultar el menú
-  }
+function copyToClipboard(text) {
+  const tempTextarea = document.createElement("textarea");
+  tempTextarea.value = text;
+  document.body.appendChild(tempTextarea);
+  tempTextarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempTextarea);
 }
