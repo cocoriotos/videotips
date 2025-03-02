4bd10e3aa207edbc72de2b4127edee5dd8336080
Julian GOnzalez <cocoriotos@hotmail.com>
2025-03-01 22:21:42 -0500

    ampresans

--- lookmigrration/index.php
+++ lookmigrration/index.php
@@ -82,7 +82,7 @@
                     <p>Pasado el periodo de prueba podrás continuar disfrutando por medio de los diferentes tipos de suscripciones así: </p>
                     <p><strong style="color: blue">Trimestral $20.000 COP</strong></p>
                     <p><strong style="color: blue">Semestral  $40.000 COP</strong></p>
-                    <p><strong style="color: blue">Anual&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$60.000 COP</strong></p>
+                    <p><strong style="color: blue">Anual&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;$60.000 COP</strong></p>
                 </div>
                 <div class="feature-item">
                     <h3>Términos y Condiciones</h3>
