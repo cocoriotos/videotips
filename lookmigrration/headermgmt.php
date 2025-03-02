ccede3b185e34c275a28b82d90ad75264df36544
Julian GOnzalez <cocoriotos@hotmail.com>
2025-03-02 11:38:42 -0500

    headmgmt

--- lookmigrration/headermgmt.php
+++ lookmigrration/headermgmt.php
@@ -10,27 +10,9 @@ $name = $_SESSION['name'];
 <html lang="us"> 
   <header>
   <nav class="navbar navbar-dark bg-dark d-flex justify-content-center" id="welcome" >
-  <center><a id="welcome"  class="navbar-brand"><span class="username-style"><?php echo $name; ?></span>, éstas en tu Biblioteca de Contenidos Útiles</a></center>
+  <center><a id="welcome"  class="navbar-brand"><span class="username-style"><?php echo $name; ?></span>. Administración de Operaciones, Usuarios y Suscripciones</a></center>
   </nav>
-  <script src="copynumber.js"></script>
-  <script src="copypaypal.js"></script>
 
-  <nav class="navbar navbar-dark bg-dark d-flex justify-content-between align-items-center" >
-    <div class="d-flex">
-        <a id="headerfonts" href="suscriptionpayment.php" class="btn" style="background-color: #d0fff8; color: black; font-weight: bold;"><i class="fa fa-credit-card-alt" aria-hidden="true"></i> Suscribirse</a>
-        <a id="headerfonts" href="https://www.youtube.com/playlist?list=PLRQ5KF9igtB2GRlHLSP6Uwx1lzy387Wz5" class="btn" target="_blank" style="background-color: white; color: #c4302b; font-weight: bold; margin-right: 10px; margin-left: 20px;"><i class="fa fa-youtube-play" aria-hidden="true"></i> Tutoriales</a>
-        <a id="headerfonts" href="UCLToolManualDelUsuario2025.pdf" class="btn" target="_blank" style="background-color:  white; color: black; font-weight: bold; margin-right: 10px; margin-left: 20px;"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> Manual</a>
-    </div>
-    <!--<div class="d-flex">
-        <a id="headerfonts" href="https://www.youtube.com/playlist?list=PLRQ5KF9igtB2GRlHLSP6Uwx1lzy387Wz5" class="btn" target="_blank" style="background-color: white; color: #c4302b; font-weight: bold; margin-right: 10px; margin-left: 20px;"><i class="fa fa-youtube-play" aria-hidden="true"></i> Tutoriales</a>
-        <a id="headerfonts" href="UCLToolManualDelUsuario2025.pdf" class="btn" target="_blank" style="background-color:  white; color: black; font-weight: bold; margin-right: 10px; margin-left: 20px;"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> Manual del Usuario</a>
-    </div>-->
-    <div class="d-flex">
-        <a id="headerfonts" href="videolinkadminmodule.php" class="btn" style="background-color: #FFF9CC; color: black; font-weight: bold; margin-left: 10px;"><i class="fas fa-broom"></i> Limpiar Formulario</a>
-        <a id="headerfonts" href="addcategory.php" class="btn" style="background-color: #D6EEFF; color: black; font-weight: bold; margin-left: 10px;"><i class="fas fa-layer-group"></i> Categorías</a>
-        <a id="headerfonts" href="videotrackerauth.php" class="btn btn-danger" style="background-color: #FFD6D6; color: black; font-weight: bold; margin-left: 10px; margin-right: 20px;"><i class="fas fa-sign-out-alt"></i> Salir</a>
-      </div>
-</nav>
 
 
   <!-- BOOTSTRAP -->	
