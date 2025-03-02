fb765c8a5a0dc58ace677f7c0a127cb047fb48e8
Julian GOnzalez <cocoriotos@hotmail.com>
2025-03-02 00:28:28 -0500

    documentation

--- lookmigrration/header.php
+++ lookmigrration/header.php
@@ -20,8 +20,8 @@ $name = $_SESSION['name'];
         <a id="headerfonts" href="suscriptionpayment.php" class="btn" style="background-color: #d0fff8; color: black; font-weight: bold;"><i class="fa fa-credit-card-alt" aria-hidden="true"></i> Suscribirse</a>
     </div>
     <div class="d-flex">
-        <a id="headerfonts" href="https://www.youtube.com/playlist?list=PLRQ5KF9igtB2GRlHLSP6Uwx1lzy387Wz5" class="btn" style="background-color: #d0fff8; color: black; font-weight: bold; margin-right: 10px; margin-left: 20px;"><i class="fa fa-youtube-play" aria-hidden="true"></i>Tutoriales</a>
-        <a id="headerfonts" href="UCLToolManualDelUsuario2025.pdf" class="btn" style="background-color: #d0fff8; color: black; font-weight: bold; margin-right: 10px; margin-left: 20px;"><i class="fa fa-file-pdf-o" aria-hidden="true"></i>Manual del Usuario</a>
+        <a id="headerfonts" href="https://www.youtube.com/playlist?list=PLRQ5KF9igtB2GRlHLSP6Uwx1lzy387Wz5" class="btn" target="_blank" style="background-color: #d0fff8; color: black; font-weight: bold; margin-right: 10px; margin-left: 20px;"><i class="fa fa-youtube-play" aria-hidden="true"></i>Tutoriales</a>
+        <a id="headerfonts" href="UCLToolManualDelUsuario2025.pdf" class="btn" target="_blank" style="background-color: #d0fff8; color: black; font-weight: bold; margin-right: 10px; margin-left: 20px;"><i class="fa fa-file-pdf-o" aria-hidden="true"></i>Manual del Usuario</a>
     </div>
     <div class="d-flex">
         <a id="headerfonts" href="videolinkadminmodule.php" class="btn" style="background-color: #FFF9CC; color: black; font-weight: bold; margin-left: 10px;"><i class="fas fa-broom"></i> Limpiar Formulario</a>
