050ccf909b18681a2a3247c841e5c04a374c0a2d
Julian GOnzalez <cocoriotos@hotmail.com>
2025-03-02 11:41:34 -0500

    headermgmt

--- lookmigrration/headermgmt.php
+++ lookmigrration/headermgmt.php
@@ -11,6 +11,7 @@ $name = $_SESSION['name'];
   <header>
   <nav class="navbar navbar-dark bg-dark d-flex justify-content-center" id="welcome" >
   <center><a id="welcome"  class="navbar-brand"><span class="username-style"><?php echo $name; ?></span>. Administración de Operaciones, Usuarios y Suscripciones</a></center>
+  <a id="headerfonts" href="videotrackerauth.php" class="btn btn-danger" style="background-color: #FFD6D6; color: black; font-weight: bold; margin-left: 10px; margin-right: 20px;"><i class="fas fa-sign-out-alt"></i> Salir</a>
   </nav>
 
 
