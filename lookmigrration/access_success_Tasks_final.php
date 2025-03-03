d318fa19bc1cf00f6ab61d4757f18db5db013d19
Julian GOnzalez <cocoriotos@hotmail.com>
2025-03-03 17:50:41 -0500

    1

--- lookmigrration/access_success_Tasks_final.php
+++ lookmigrration/access_success_Tasks_final.php
@@ -104,7 +104,7 @@ $password=$_POST['password'];
 				{
 					$query10="select adm_role from videotips_app_access_list where email='$local_username' and active='1' and password='$password'"; 
 					$admrole=mysqli_query($conn, $query10);
-					if ($admrole == true) {
+					if ($admrole == 1) {
 						header("refresh:0; url=AppMgmt.php");
 						exit();
 						} else {
