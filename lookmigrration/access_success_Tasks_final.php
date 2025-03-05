ae66b0265fe51504f76834ccd23e24b7468068f9
Julian GOnzalez <cocoriotos@hotmail.com>
2025-03-04 22:49:38 -0500

    test wrong db

--- lookmigrration/access_success_Tasks_final.php
+++ lookmigrration/access_success_Tasks_final.php
@@ -88,7 +88,7 @@ $password=$_POST['password'];
   $db_host="127.0.0.1";
   $db_user="u927778197_adm";
   $db_pass="C0mp13t3501ut10n5*";
-  $db_name="u927778197_appsdb";
+  $db_name="u927778197_appsdb1";
   $conn=mysqli_connect($db_host,$db_user,$db_pass,$db_name);
 		if(mysqli_connect_errno()) 
 					{	
