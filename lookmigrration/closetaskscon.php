05c459fd2ecef85c44a1f1056a4bad3bbec84bec
Julian GOnzalez <cocoriotos@hotmail.com>
2025-03-04 23:47:03 -0500

    sessions

--- lookmigrration/closetaskscon.php
+++ lookmigrration/closetaskscon.php
@@ -1,7 +1,32 @@
 <?php 
-        session_unset(); 
+        /*session_unset(); 
 		session_destroy(); 
 		mysqli_close($conn);
         header("Location: videotrackerauth.php");
-		exit();
+		exit();*/
+		// Iniciar la sesión si no está iniciada
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Verificar el estado de la sesión
+switch (session_status()) {
+    case PHP_SESSION_DISABLED:
+        echo "Las sesiones están deshabilitadas en el servidor.";
+        break;
+    case PHP_SESSION_NONE:
+        echo "No hay una sesión activa.";
+        break;
+    case PHP_SESSION_ACTIVE:
+        echo "Hay una sesión activa.";
+        // Puedes imprimir las variables de sesión si lo deseas
+        if (!empty($_SESSION)) {
+            echo "<pre>Variables de sesión: ";
+            print_r($_SESSION);
+            echo "</pre>";
+        } else {
+            echo "No hay variables de sesión definidas.";
+        }
+        break;
+}
 ?>
