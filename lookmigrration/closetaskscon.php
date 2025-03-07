d9bab3ed458452ad3cc6a5e1fb5a88c469cacccf
Julian GOnzalez <cocoriotos@hotmail.com>
2025-03-06 23:51:12 -0500

    ok

--- lookmigrration/closetaskscon.php
+++ lookmigrration/closetaskscon.php
@@ -1,15 +1,10 @@
 <?php 
-        /* 
-		session_destroy(); 
-		mysqli_close($conn);
-        header("Location: videotrackerauth.php");
-		exit();*/
-		// Iniciar la sesión si no está iniciada
 	session_cache_limiter('nocache');
     session_start();
 	session_unset();
 	session_destroy();
 	$conn->close();
-	header("videotrackerauth.php");
+
+	//header("videotrackerauth.php");
 	exit();
 ?>
